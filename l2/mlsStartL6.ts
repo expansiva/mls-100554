696d8cb442d430a0cd634216445c4088edf1bd61
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-10-05 12:09:03 -0300

    Save mls file:l2/mlsStartL6.ts

---
+++ l2/mlsStartL6.ts
@@ -0,0 +1,11 @@
+/// <mls shortName="mlsStartL6" project="100554" enhancement="_100541_enhancementLit" />
+				
+import { html, LitElement} from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('mls-start-l6-100554')
+export class MLSStartL6 extends LitElement {
+    render() {
+        return html` <h3>Teste Pagina</h3> `;
+    }
+}
