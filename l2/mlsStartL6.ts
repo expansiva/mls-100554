edf57d2a65f04b979f9683710da7353ec04971d2
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-10-05 16:49:26 -0300

    Save mls file:l2/mlsStartL6.ts

--- l2/mlsStartL6.ts
+++ l2/mlsStartL6.ts
@@ -22,3 +22,26 @@ export class MLSStartL6 extends HTMLElement {
 }
 
 customElements.define('mls-start-l6-100554', MLSStartL6);
+
+/* import { html, unsafeHTML, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('mls-start-l6-100554')
+export class MLSStartL6 extends LitElement {
+
+    async getHTMLFile() {
+        const key = mls.stor.getKeyToFiles(100554, 2, 'mlsStartL6', '', '.html');
+        const storFileHTML = mls.stor.files[key];
+        let src: string | Blob;
+        const info: mls.stor.IFileInfoValue | null = storFileHTML.getValueInfo ? await storFileHTML.getValueInfo() : null;
+        const haveInfo: boolean = info && !!info.content;
+        src = haveInfo ? info.content : await storFileHTML.getContent();
+        return src;
+    }
+
+    async render() {
+        const src = await this.getHTMLFile() as string;
+        return html`${unsafeHTML(src)}`;
+    }
+}
+ */
