08f4eaeb820c5c2fe0a7586a3aca529238adaf4b
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-10-09 14:54:52 -0300

    Save mls file:l2/mlsStartL4.ts

--- l2/mlsStartL4.ts
+++ l2/mlsStartL4.ts
@@ -27,8 +27,7 @@ export class MLSStartL4 extends LitElement {
   }
 
     async getHTMLFile() {
-        return 'teste';
-    const key = mls.stor.getKeyToFiles(100554, 2, 'mlsStartL5', '', '.html');
+    const key = mls.stor.getKeyToFiles(100554, 2, 'mlsStartL4', '', '.html');
     const storFileHTML = mls.stor.files[key];
     let src: string | Blob;
     const info: mls.stor.IFileInfoValue | null = storFileHTML.getValueInfo ? await storFileHTML.getValueInfo() : null;
