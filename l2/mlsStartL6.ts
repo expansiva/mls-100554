9eef17e753795ea59ec09d941a4417a92b2b1450
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-10-09 08:21:34 -0300

    Save mls file:l2/mlsStartL6.ts

--- l2/mlsStartL6.ts
+++ l2/mlsStartL6.ts
@@ -1,36 +1,31 @@
 /// <mls shortName="mlsStartL6" project="100554" enhancement="_100541_enhancementLit" groupName="internal" />
 
-/* export class MLSStartL6 extends HTMLElement {
-    
-    async getHTMLFile() {
-        const key = mls.stor.getKeyToFiles(100554, 2, 'mlsStartL6', '', '.html');
-        const storFileHTML = mls.stor.files[key];
-        let src: string | Blob;
-        const info: mls.stor.IFileInfoValue | null = storFileHTML.getValueInfo ? await storFileHTML.getValueInfo() : null;
-        const haveInfo: boolean = info && !!info.content;
-        src = haveInfo ? info.content : await storFileHTML.getContent();
-        return src;
-    }
-
-    connectedCallback() {
-        this.render();
-    }
-
-    async render() {
-        const src = await this.getHTMLFile() as string;
-        return this.innerHTML = src;
-    }
-}
- 
-customElements.define('mls-start-l6-100554', MLSStartL6);
- */
-
 import { html, unsafeHTML, css, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 
 @customElement('mls-start-l6-100554')
 export class MLSStartL6 extends LitElement {
 
+    @property({ type: String }) html: string | null = null;
+    @property({ type: Boolean }) loading: boolean = true;
+    @property({ type: String }) msize: string | null = null;
+
+    static get styles() {
+    return css`
+      :host {
+        display:block;
+        overflow: auto;
+      }
+    `;
+  }
+
+    async connectedCallback() {
+        super.connectedCallback();
+        await this.getHTMLFile();
+        this.loading = false;
+        this.requestUpdate(); // Trigger a re-render
+    }
+
     async getHTMLFile() {
         const key = mls.stor.getKeyToFiles(100554, 2, 'mlsStartL6', '', '.html');
         const storFileHTML = mls.stor.files[key];
@@ -38,13 +33,25 @@ export class MLSStartL6 extends LitElement {
         const info: mls.stor.IFileInfoValue | null = storFileHTML.getValueInfo ? await storFileHTML.getValueInfo() : null;
         const haveInfo: boolean = info && !!info.content;
         src = haveInfo ? info.content : await storFileHTML.getContent();
-        return src;
+        this.html = src as string;
     }
 
-    async render() {
-        // const src = await this.getHTMLFile() as string;
-        // return html`${unsafeHTML(src)}`;
-        return html`teste`;
+    attributeChangedCallback(name:string, oldVal:string, newVal:string) {
+        super.attributeChangedCallback(name, oldVal, newVal);
+        if (name === 'msize') {
+            const [width, height] = newVal.split(',');
+			this.style.height = height + 'px';
+        }
+    }
+
+    render() {
+        return html`
+      <div>
+        ${this.loading
+                ? html`<p>Loading...</p>`
+                : html`<div>${unsafeHTML(this.html)}</div>`}
+      </div>
+    `;
 
 
     }
