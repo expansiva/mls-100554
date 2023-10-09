aaf60d3c66a2ed999757946a4fbf6663aeb7b4ee
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-10-09 15:04:11 -0300

    Save mls file:l2/mlsStartL3.ts

---
+++ l2/mlsStartL3.ts
@@ -0,0 +1,59 @@
+/// <mls shortName="mlsStartL3" project="100554" enhancement="_100541_enhancementLit" />
+
+import { html, unsafeHTML, css, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+@customElement('mls-start-l3-100554')
+export class MLSStartL3 extends LitElement {
+
+    @property({ type: String }) html: string | null = null;
+    @property({ type: Boolean }) loading: boolean = true;
+    @property({ type: String }) msize: string | null = null;
+
+    static get styles() {
+        return css`
+      :host {
+        display:block;
+        overflow: auto;
+      }
+    `;
+    }
+
+    async connectedCallback() {
+        super.connectedCallback();
+        await this.getHTMLFile();
+        this.loading = false;
+        this.requestUpdate(); // Trigger a re-render
+    }
+
+    async getHTMLFile() {
+        return 'reste';
+        const key = mls.stor.getKeyToFiles(100554, 2, 'mlsStartL4', '', '.html');
+        const storFileHTML = mls.stor.files[key];
+        let src: string | Blob;
+        const info: mls.stor.IFileInfoValue | null = storFileHTML.getValueInfo ? await storFileHTML.getValueInfo() : null;
+        const haveInfo: boolean = info && !!info.content;
+        src = haveInfo ? info.content : await storFileHTML.getContent();
+        this.html = src as string;
+    }
+
+    attributeChangedCallback(name: string, oldVal: string, newVal: string) {
+        super.attributeChangedCallback(name, oldVal, newVal);
+        if (name === 'msize') {
+            const [width, height] = newVal.split(',');
+            this.style.height = height + 'px';
+        }
+    }
+
+    render() {
+        return html`
+      <div>
+        ${this.loading
+                ? html`<p>Loading...</p>`
+                : html`<div>${unsafeHTML(this.html)}</div>`}
+      </div>
+    `;
+
+
+    }
+}
