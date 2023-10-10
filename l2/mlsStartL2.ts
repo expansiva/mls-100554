515c1d600c614e91259a99f01ed56c4d82ab18ea
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2023-10-10 14:13:39 -0300

    Save mls file:l2/mlsStartL2.ts

--- l2/mlsStartL2.ts
+++ l2/mlsStartL2.ts
@@ -1,57 +1,69 @@
 /// <mls shortName="mlsStartL2" project="100554" enhancement="_100541_enhancementLit" />
 
 import { html, unsafeHTML, css, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, query } from 'lit/decorators.js';
 
 @customElement('mls-start-l2-100554')
 export class MLSStartL2 extends LitElement {
 
-    @property({ type: String }) html: string | null = null;
-    @property({ type: Boolean }) loading: boolean = true;
-    @property({ type: String }) msize: string | null = null;
+  @property({ type: String }) html: string | null = null;
+  @property({ type: Boolean }) loading: boolean = true;
+  @property({ type: String }) msize: string | null = null;
 
-    static get styles() {
-        return css`
+  @query('#l2check') check:HTMLInputElement;
+
+  static get styles() {
+    return css`
       :host {
         display:block;
         overflow: auto;
       }
     `;
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+    await this.getHTMLFile();
+    this.loading = false;
+    this.requestUpdate(); // Trigger a re-render
+  }
+
+  async getHTMLFile() {
+
+    const key = mls.stor.getKeyToFiles(100554, 2, 'mlsStartL2', '', '.html');
+    const storFileHTML = mls.stor.files[key];
+    let src: string | Blob;
+    const info: mls.stor.IFileInfoValue | null = storFileHTML.getValueInfo ? await storFileHTML.getValueInfo() : null;
+    const haveInfo: boolean = info && !!info.content;
+    src = haveInfo ? info.content : await storFileHTML.getContent();
+    this.html = src as string;
+  }
+
+  attributeChangedCallback(name: string, oldVal: string, newVal: string) {
+    super.attributeChangedCallback(name, oldVal, newVal);
+    if (name === 'msize') {
+      const [width, height] = newVal.split(',');
+      this.style.height = height + 'px';
     }
+  }
+  changeStatusService() {
+    let data: boolean[] = [true, true, true, true, true, true, true, true]
+    const dataStr = localStorage.getItem('collabcodes-showstart');
+    if (dataStr) data = JSON.parse(dataStr);
+    data[2] = this.check.checked;
+    localStorage.setItem('collabcodes-showstart', JSON.stringify(data));  
+  }
 
-    async connectedCallback() {
-        super.connectedCallback();
-        await this.getHTMLFile();
-        this.loading = false;
-        this.requestUpdate(); // Trigger a re-render
-    }
-
-    async getHTMLFile() {
-
-        const key = mls.stor.getKeyToFiles(100554, 2, 'mlsStartL2', '', '.html');
-        const storFileHTML = mls.stor.files[key];
-        let src: string | Blob;
-        const info: mls.stor.IFileInfoValue | null = storFileHTML.getValueInfo ? await storFileHTML.getValueInfo() : null;
-        const haveInfo: boolean = info && !!info.content;
-        src = haveInfo ? info.content : await storFileHTML.getContent();
-        this.html = src as string;
-    }
-
-    attributeChangedCallback(name: string, oldVal: string, newVal: string) {
-        super.attributeChangedCallback(name, oldVal, newVal);
-        if (name === 'msize') {
-            const [width, height] = newVal.split(',');
-            this.style.height = height + 'px';
-        }
-    }
-
-    render() {
-        return html`
+  render() {
+    return html`
       <div>
         ${this.loading
-                ? html`<p>Loading...</p>`
-                : html`<div>${unsafeHTML(this.html)}</div>`}
+        ? html`<p>Loading...</p>`
+        : html`
+        <div><input id="l2check" checked type="checkbox" @change="${this.changeStatusService}" ></input> Mostrar este service na primeira vez que entrar neste módulo </div>
+        <div>${unsafeHTML(this.html)}</div>
+        `}
       </div>
     `;
-    }
+  }
 }
