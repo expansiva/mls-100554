df9e49fedebe69bb5f309b9ba34b2fda6dc26648
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2024-03-11 14:54:45 -0300

    Update serviceListFiles.ts

--- l2/serviceListFiles.ts
+++ l2/serviceListFiles.ts
@@ -32,7 +32,7 @@ export class ServiceListFiles extends ServiceBase {
         super();
         initServiceListFilesAdd();
         this.setEvents();
-	console.info('2')
+	console.info('3')
     }
 
     static styles = css`[[mls_getDefaultDesignSystem]]`;
