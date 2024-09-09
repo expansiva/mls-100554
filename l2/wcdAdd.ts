1df93006b7854b296e216776160062469daa841c
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2024-09-09 14:09:33 -0300

    Fix bug wcdAdd icon position
    Fix bug wcdAdd icon position

--- l2/wcdAdd.ts
+++ l2/wcdAdd.ts
@@ -178,7 +178,6 @@ export class WcdAdd100554 extends WcdToolboxItemBase {
             height: 32px;
             line-height: 30px;
             padding: 0;
-            font-size: 15px;
             background: var(--bg-primary-color-lighter);
             border-radius: 100%;
             border: 1px solid rgba(0,0,0,.68);
