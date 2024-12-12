fdfef17a292ea33efd6b3067fde11e811e51e9e5
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2024-12-12 13:54:14 -0300

    implements pluginLessPseudo
    implements pluginLessPseudo

--- l2/pluginStyleMargin.ts
+++ l2/pluginStyleMargin.ts
@@ -82,6 +82,8 @@ export class PluginStyleSpacing extends IcaLitElement {
     handleIcaStateChange(_key: string, _value: ICSSState) {
         if (_key !== `less.${this.position}` || !_value) return;
         if (_value.emitter === 'helper') return;
+        if (!_value.key || !_value.key.startsWith('margin')) return;
+
         this._onIcaStateChange();
     }
 
