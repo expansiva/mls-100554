9cf1ea7ab295b00b51d57621401811ee7781a102
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-01-17 09:53:24 -0300

    Voltando 'teste'
    Voltando 'teste'

--- l2/ateste.ts
+++ l2/ateste.ts
@@ -1,107 +1,32 @@
 /// <mls shortName="ateste" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
 
-import { html, LitElement, repeat } from 'lit';
+import { html, css, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import './_100554_wcAuxCommand';
+import { getOrCreateDSInstanceIO } from './_100554_libDesignSystem'
+import { CollabLitElement } from './_100554_collabLitElement'
+import { IcaLitElement } from './_100554_icaLitElement'
 
+// teste 5 
+const message_ru = { installPlugin: 'Установить плагин', createNewPlugin: 'Создать новый плагин', backList: 'Вернуться к списку', noPluginsInstalled: 'Плагины не установлены', desactivate: 'Деактивировать', activate: 'Активировать', delete: 'Удалить', reference: 'Справка', noPluginsAvaliables: 'Нет доступных плагинов', install: 'Установить', p1: 'Что такое плагины?', p2: 'Плагины - это фрагменты кода, которые добавляют дополнительные функции в ваш проект. Они разработаны для расширения и улучшения возможностей вашего проекта.', p3: 'Как работают плагины?', p4: 'Когда вы устанавливаете и активируете плагин, он добавляет новые функции или возможности в ваш проект. Плагины могут изменять способ работы вашего проекта, добавляя новые параметры конфигурации, искусственный интеллект, виджеты, короткие коды и другие функции.', p5: 'Где найти плагины?', p6: 'Вы можете найти плагины непосредственно в (L5) вашего проекта, в разделе Сервисы (Services), называемом "Плагины". Здесь вы можете управлять и добавлять новые плагины в свой проект.', p7: 'Как создать плагин?', p8: 'Для создания плагина...', }
 
 @customElement('ateste-100554')
-export class SimpleGreeting extends LitElement {
-
+export class SimpleGreeting extends CollabLitElement {
+    
     createRenderRoot() {
         return this;
     }
 
-
-    @property() tot: number = 10;
-    @property() list: string[] = [];
-
+        
+    @property() name: string = new Date(Date.now()).toString();
+    
     handleConfirm(e: CustomEvent) {
         console.info(e.detail)
     }
 
-    connectedCallback() {
-        super.connectedCallback();
-        this.prepareList();
-    }
-
-    private listComands = [
-        {
-            group: 'Group 1',
-            itens: [
-                {
-                    label: 'Home',
-                    value: 'home',
-                    icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>'
-                },
-                {
-                    label: 'Config',
-                    value: 'config',
-                    icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>'
-                }
-            ]
-        },
-        {
-            group: 'Group 2',
-            itens: [
-                {
-                    label: 'Home',
-                    value: 'home',
-                    icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>'
-                },
-                {
-                    label: 'Config',
-                    value: 'config',
-                    icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>'
-                },
-                {
-                    label: 'Code',
-                    value: 'code',
-                    icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>'
-                }
-            ]
-        }
-
-    ]
-
     render() {
 
-        return html`
-            <input type="text" id="idteste" autocomplete="off"></input>
-            <wc-aux-command-100554 idElView="idteste" caracter="/" .itens=${this.listComands} @clickitem=${this.clickItem}/>
-        `;
-
+        return html`<div class="cls1"><h1>Hello world lucas 2</h1></div>`;
     }
-
-    private clickItem(e: MouseEvent) {
-        console.info(e.detail);
-    }
-
-    /*render() {
-
-        return html`
-        <ul>
-            ${repeat(this.list, (
-                (key: string) => key) as any,
-                ((k: any, index: any) => { return html`<li>${k}</li>` }) as any)
-            }
-        </ul>
-        `; 
-        
-    }*/
-
-    private prepareList() {
-
-        const newList = [];
-        for (let i = 0; i < this.tot; i++) {
-            newList.push(i.toString());
-        }
-
-        this.list = newList;
-
-    }
-
-
 
 
 }
