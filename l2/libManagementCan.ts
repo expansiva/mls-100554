b8d5172be987d64ace4eeb740c3deb4a0c90e4bd
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-02-20 16:22:22 -0300

    implements function setState and verifyState
    implements function setState and verifyState

--- l2/libManagementCan.ts
+++ l2/libManagementCan.ts
@@ -1,13 +1,30 @@
 /// <mls shortName="libManagementCan" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
 
-export function addTest(path: string, fc: Function) {
-
-}
-
 export function setState(path: string, value: any) {
-
+    if (!window.preview.iframe) throw new Error('Invalid preview iframe');
+    if (!window.preview.iframe.contentWindow) throw new Error('Invalid preview iframe contentWindow');
+    if (!((window.preview.iframe.contentWindow as any)['globalStateManagment'])) throw new Error('Invalid preview stateManagment');
+    try {
+        (window.preview.iframe.contentWindow as any)['globalStateManagment'].setValue(path, value);
+        return true;
+    } catch (err: any) {
+        throw new Error(err.message);
+    }
 }
 
 export function verifyState(path: string, value: any) {
 
+    if (!window.preview.iframe) throw new Error('Invalid preview iframe');
+    if (!window.preview.iframe.contentWindow) throw new Error('Invalid preview iframe contentWindow');
+    if (!((window.preview.iframe.contentWindow as any)['globalStateManagment'])) throw new Error('Invalid preview stateManagment');
+    try {
+        let oldValue = (window.preview.iframe.contentWindow as any)['globalStateManagment'].getValue(path);
+        let newValue = value;
+        if (typeof newValue === 'object') newValue = JSON.stringify(value);
+        if (typeof oldValue === 'object') newValue = JSON.stringify(oldValue);
+        if (oldValue !== newValue) throw new Error(`Test failed = result: ${newValue} expect: ${oldValue}`);
+        return true;
+    } catch (err: any) {
+        throw new Error(err.message);
+    }
 }
