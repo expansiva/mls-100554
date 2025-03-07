a6af80003b4742f0c3b01ad55f7e57f78550784e
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-03-07 07:49:21 -0300

    Correção posicionamento.
    Correção posicionamento.

--- l2/wcdToolboxItemActionEditAttrDialog.ts
+++ l2/wcdToolboxItemActionEditAttrDialog.ts
@@ -1,8 +1,8 @@
 /// <mls shortName="wcdToolboxItemActionEditAttrDialog" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
 
 import { html } from 'lit';
-import { customElement,query } from 'lit/decorators.js';
-import {IListWidgetBase } from './_100554_wcdTypes';
+import { customElement, query } from 'lit/decorators.js';
+import { IListWidgetBase } from './_100554_wcdTypes';
 import { CollabLitElement } from './_100554_collabLitElement'
 import { globalWcd } from './_100554_wcdState';
 import './_100554_wcdToolboxItemActionEditAttrOut';
@@ -13,7 +13,7 @@ export class WcdToolboxItemActionEditAttrDialog extends CollabLitElement {
     private lastIca: HTMLElement | undefined;
     private lastHeight: string | undefined;
     private lastHeightWcd: string | undefined;
-    @query('wcd-toolbox-item-action-edit-attr-out-100554') itemEdit:HTMLElement | undefined
+    @query('wcd-toolbox-item-action-edit-attr-out-100554') itemEdit: HTMLElement | undefined
 
     //-------COMPONENT----------
 
@@ -26,44 +26,78 @@ export class WcdToolboxItemActionEditAttrDialog extends CollabLitElement {
         }
         if (globalWcd.elICA) globalWcd.elICA.style.height = this.lastHeight || '';
         else if (this.lastIca) this.lastIca.style.height = this.lastHeight || '';
+        if (this.resizeObserver) this.resizeObserver.disconnect();
+
         super.disconnectedCallback();
     }
 
     firstUpdated(changedProperties: Map<string | number | symbol, unknown>) {
         super.firstUpdated(changedProperties);
+        this.initObserverResize();
     }
 
     updated(changedProperties: Map<string | number | symbol, unknown>) {
         super.updated(changedProperties);
-        
+
     }
 
     render() {
 
         setTimeout(() => this.recalculeIcaHeight(), 200);
-        this.style.cssText = `width: 100%; left:0px` ;
+        this.style.cssText = `width: 100%; left:0px`;
         return html`<wcd-toolbox-item-action-edit-attr-out-100554></wcd-toolbox-item-action-edit-attr-out-100554>`;
     }
 
     //------IMPLEMENTS----------
 
+    private resizeObserver: ResizeObserver | undefined;
+    private timeResize = 0;
+    private lastHeightResize = 0;
+    private firstHeight = 0;
+    private initObserverResize() {
+
+        if (!this) return;
+
+        if (this.resizeObserver) this.resizeObserver.disconnect();
+        this.resizeObserver = new ResizeObserver(entries => {
+
+            if (!entries || !entries[0] || !entries[0].contentRect) return;
+            const actual = entries[0].contentRect.height;
+
+            if (actual === this.lastHeightResize) return;
+
+            this.lastHeightResize = actual;
+            clearTimeout(this.timeResize);
+
+            this.timeResize = setTimeout(() => {
+                this.recalculeIcaHeight();
+            }, 100)
+
+        });
+        this.resizeObserver.observe(this);
+    }
+
     private recalculeIcaHeight() {
+        
         if (!globalWcd) throw new Error('Invalid window.wcdState');
         if (!globalWcd.elICA) throw new Error('Invalid window.wcdState.elICA');
         if (!this.itemEdit) return;
 
         const heighttot = this.itemEdit.getBoundingClientRect()?.height;
         const height = (globalWcd.elICA as any).getBoundingClientRect()?.height;
-        
-        if (this.lastHeight === undefined) this.lastHeight = (globalWcd.elICA as any).style.height;
+
+        if (this.lastHeight === undefined) {
+            this.lastHeight = (globalWcd.elICA as any).style.height;
+            this.firstHeight = height;
+        }
         if (this.lastHeightWcd === undefined) this.lastHeightWcd = (globalWcd.myParent as any).style.height;
 
         (globalWcd.myParent as any).setAttribute('needresize', 'false');
         //(globalWcd.myParent as any).style.overflowY = 'auto';
-        (globalWcd.myParent as any).style.height = heighttot+'px';
-        (globalWcd.elICA as any).style.height = (heighttot + height) + 'px';
-        
-        (globalWcd.myParent as any).style.top = height + 5 + 'px'; 
+        (globalWcd.myParent as any).style.height = heighttot + 'px';
+        (globalWcd.elICA as any).style.height = (heighttot + this.firstHeight + 30) + 'px';
+
+        (globalWcd.myParent as any).style.top = this.firstHeight + 5 + 'px';
     }
 
 }
