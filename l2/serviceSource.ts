ab0c37081cb79e1edb6fc7157b4637ef49a1b87d
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-03-11 12:09:38 -0300

    Fix bug new File
    Fix bug new File

--- l2/serviceSource.ts
+++ l2/serviceSource.ts
@@ -1107,7 +1107,7 @@ export class ServiceSource100554 extends ServiceBase {
         storFile.hasError = hasError;
         this.toogleIconsError();
         const sameContent: boolean = modelBaseTS.originalCRC === mls.common.crc.crc32(modelBaseTS.model.getValue()).toString(16);
-        if (sameContent) {
+        if (sameContent && changed) {
             if (storFile.status !== 'new') storFile.status = 'nochange';
             await mls.stor.localStor.setContent(storFile, { content: null }); // clear localstorage
         } else {
