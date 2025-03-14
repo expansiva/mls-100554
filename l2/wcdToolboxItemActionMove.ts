74f2e4d25257aa8dc3b94bbbae53ba4e94b1d9b6
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-03-14 18:53:37 -0300

    Init move preview
    Init move preview

--- l2/wcdToolboxItemActionMove.ts
+++ l2/wcdToolboxItemActionMove.ts
@@ -1,293 +1,118 @@
 /// <mls shortName="wcdToolboxItemActionMove" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
-import { html, LitElement } from 'lit';
+import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import {WCDToolboxMethodos} from './_100554_wcdTypes';
-import {IcaLitElementBaseMethods, IActionsToolbox} from './_100554_icaTypes';
+import { WcdToolboxItemBase } from './_100554_wcdToolboxItemBase';
 
-//version 4
 @customElement('wcd-toolbox-item-action-move-100554')
-export class WCDToolboxItemActionMove extends LitElement {
+export class WCDToolboxItemActionMove extends WcdToolboxItemBase {
 
-    public myParent: WCDToolboxMethodos | undefined;
-    public elMain: HTMLElement | undefined;
-    public elFCA: IcaLitElementBaseMethods | undefined;
-    private myElements: IcaLitElementBaseMethods[] | undefined;
+    public args: string | undefined;
 
-    createRenderRoot() {
-        return this;
+    constructor() {
+        super();
+    }
+
+    //-------COMPONENT---------------------
+
+    updated(changedProperties: any) {
+        super.updated(changedProperties);
+
     }
 
     render() {
+        if (this.args === 'left') return this.renderLeft();
+        if (this.args === 'below') return this.renderBelow();
+        return this.renderButton();
+        
+    }
 
-        return html``;
+    renderButton() {
+        this.title = "move";
+        this.onclick = (e) => this.clickButton(e);
+        this.classList.add('f-button');
+        return html`<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M278.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l9.4-9.4V224H109.3l9.4-9.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-9.4-9.4H224V402.7l-9.4-9.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-9.4 9.4V288H402.7l-9.4 9.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3l-64-64c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l9.4 9.4H288V109.3l9.4 9.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-64-64z"/></svg>`;
+    }
+
+    renderLeft() {
+        this.title = "move";
+        this.style.left = '-30px';
+        this.onclick = (e) => this.clickButton(e);
+        return html`
+            <svg xmlns="http://www.w3.org/2000/svg" style="width: 15px; height: 15px; background: #0099ff; padding: 3px; fill: white; border-radius: 50%;" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
+        `
+    }
+
+    renderBelow() {
+        this.title = "move";
+        this.style.bottom = '-30px';
+        this.onclick = (e) => this.clickButton(e);
+        return html`
+            <svg xmlns="http://www.w3.org/2000/svg" style="transform: rotate(90deg); width: 15px; height: 15px; background: #0099ff; padding: 3px; fill: white; border-radius: 50%;" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
+        `
+    }
+
+    //---------IMPLEMENTATION-----------------
+
+    private clickButton(e: MouseEvent) {
+
+        e.stopPropagation();
+
+        if (!this.myParent || !this.elICA) return;
+
+        const s = this.checkSiblingsPosition(this.elICA);
+
+        if (s === 'nosiblings') return;
+
+        const p = s === 'left' ? 'p-l2' : 'p-m3';
+
+        this.myParent.setIconsWcdToolbox(
+            [
+                {
+                    name: 'backButton'
+                },
+                {
+                    name: '_100554_wcdToolboxItemActionMove',
+                    level: [2,3],
+                    args: s,
+                    position: p
+                }
+            ],
+            false,
+            'false'
+        )
+
 
     }
 
-    updated(changedProperties: any) {
 
-        super.updated(changedProperties);
+    private checkSiblingsPosition(element: HTMLElement): 'nosiblings' | 'below' | 'left' {
 
-        this.onclick = (event) => { this.initClick(event); };
+        if (!element.parentElement) return 'nosiblings';
 
-    }
+        const siblings = Array.from(element.parentElement.children).filter(el => el !== element);
 
-    public beforeRemove(): void {
+        if (siblings.length === 0) return 'nosiblings';
 
-        if (!this.myElements) return;
-        document.body.onmouseup = () => { };
-        this.myElements.forEach((i) => {
-            this.changeStateDrop(i);
-        });
+        const elementRect = element.getBoundingClientRect();
+        let hasHorizontalSibling = false;
+        let hasVerticalSibling = false;
 
-    }
+        for (const sibling of siblings) {
+            const siblingRect = sibling.getBoundingClientRect();
 
-    private clearDrag(): void {
-
-        if (!this.myElements) return;
-        this.myElements.forEach((i) => {
-            this.changeStateDrop(i);
-        });
-
-    }
-
-    private stopDragging(e: MouseEvent): void {
-
-        try {
-            e.stopPropagation();
-            if (!this.myParent || !this.elFCA) return;
-
-            const myGrandFather = this.myParent.parentElement as IcaLitElementBaseMethods;
-
-            document.body.style.cursor = '';
-
-            const aux = this.getAux(e.target as HTMLElement);
-            if (!aux || !(aux as any).elBase) {
-                this.clearDrag();
-                return;
+            if (Math.abs(siblingRect.top - elementRect.top) < 5) {
+                
+                hasVerticalSibling = true;
             }
-
-            const elBase = (aux as any).elBase;
-
-            const oldParent = myGrandFather.getIcaParent(this.elFCA);
-            const newParent = myGrandFather.getIcaParent(elBase);
-
-            let father = elBase.parentElement as HTMLElement;
-            father = father ? father : document.body as HTMLElement
-
-            const move = aux.tagName.toLocaleLowerCase();
-
-            const newEl: any = this.elFCA.cloneNode(true);
-            /// newEl.myInnerHTML = this.elFCA.myInnerHTML; ver questão filhos
-
-            switch (move) {
-                case 'wcd-dragdrop-aux-before':
-                    father.insertBefore(newEl, elBase);
-                    this.elFCA.remove();
-                    break;
-                case 'wcd-dragdrop-aux-after':
-                    father.insertBefore(newEl, elBase.nextSibling);
-                    this.elFCA.remove();
-                    break;
-                case 'wcd-dragdrop-aux-in':
-                    const elIn = elBase.querySelector(elBase.widget);
-                    if (elIn) elIn.appendChild(this.elFCA);
-                    break;
-                default:
-                    '';
+            if (Math.abs(siblingRect.left - elementRect.left) < 5) {
+                hasHorizontalSibling = true;
             }
-
-            // if (oldParent) oldParent.updateMyInnerHtmlIfNeed(false);
-            // if (newParent) newParent.updateMyInnerHtmlIfNeed(false);
-            //else {
-                newEl.setAttribute('rendertype', 'edit');
-                setTimeout(() => {
-                    newEl.click();
-                }, 100)
-
-            //}
-
-            setTimeout(() => {
-                if (this.myParent) mls.events.fire((+(this.myParent.level)) as any, 'WCDEventChange' as any, `{"op":"Navigation"}`);
-            }, 500)
-
-
-            this.clearDrag();
-
-
-        } catch (err) {
-
-            this.clearDrag();
         }
 
+        if (hasVerticalSibling) return 'below';
+        if (hasHorizontalSibling) return 'left';
+
+        return 'nosiblings';
     }
 
-    private getAux(el: HTMLElement): HTMLElement | undefined {
-
-        if (el.tagName.toLocaleLowerCase().startsWith('wcd-dragdrop-aux-')) {
-            return el;
-        }
-
-        const parent = el.parentElement;
-        if (!parent) return;
-
-        const tag = parent.tagName.toLowerCase();
-
-        if (tag.startsWith('wcd-dragdrop-aux-')) {
-
-            return parent;
-
-        }
-
-        return this.getAux(parent);
-    }
-
-    private initClick(e: MouseEvent): void {
-
-        if (!this.elMain || !this.elFCA || !this.myParent || !document.defaultView) return;
-
-        //WCD's father will always be an FCA
-        const myGrandFather = this.myParent.parentElement as IcaLitElementBaseMethods;
-
-        const scope = myGrandFather.getMyScope();
-        if (!scope) return;
-
-        this.myElements = myGrandFather.getICAComponents(scope);
-
-        this.myElements = this.onlyNeedAddTag(this.myElements);
-
-        this.myElements.forEach((i) => {
-            this.changeStateDrag(i, scope, myGrandFather);
-        });
-
-        if (!this.myParent.shadowRoot) return;
-        Array.from(this.myParent.shadowRoot.children).forEach((i) => {
-
-            const tag = i.tagName.toLocaleLowerCase();
-            if (tag !== 'wcd-toolbox-item-action-move-100554') i.remove();
-
-        })
-
-    }
-
-    
-    private onlyNeedAddTag(array: IcaLitElementBaseMethods[]): IcaLitElementBaseMethods[] {
-
-        const a: IcaLitElementBaseMethods[] = [];
-
-        for (let i = 0; i <= array.length; i++) {
-
-            const elBase = array[i];
-            const next = array[i + 1];
-            if (!next) {
-                a.push(elBase);
-                continue;
-            }
-
-            const parent = next.getIcaParent(next);
-
-            if (!parent && (!next.parentElement || next.parentElement.tagName !== 'BODY')) continue;
-            if (next.parentElement && next.parentElement.tagName === 'BODY') a.push(elBase)
-            else if (parent !== elBase) a.push(elBase);
-
-
-        }
-
-        return a;
-    }
-
-    private changeStateDrag(elBase: IcaLitElementBaseMethods, elScope: HTMLElement, elMove: IcaLitElementBaseMethods): void {
-
-        if (!elBase) return;
-        if (elBase.getAttribute('renderType') === 'editactive' || !this.myParent) return;
-
-        const valid = elBase.allowCommand('move', elScope, elMove);
-
-        if (!valid.before && !valid.after && !valid.inside) return;
-
-        const content = document.createElement('wcd-dragdrop-aux');
-
-        content.style.position = 'absolute';
-        content.style.display = 'flex';
-        content.style.gap = '1rem';
-        content.style.justifyContent = 'center';
-        content.style.alignItems = 'center';
-        content.style.background = '#0c66e461';
-
-        const before = document.createElement('wcd-dragdrop-aux-before');
-        const after = document.createElement('wcd-dragdrop-aux-after');
-        const inn = document.createElement('wcd-dragdrop-aux-in');
-
-        before.onclick = (e: MouseEvent) => this.stopDragging(e);
-        after.onclick = (e: MouseEvent) => this.stopDragging(e);
-        inn.onclick = (e: MouseEvent) => this.stopDragging(e);
-
-        (before as any).elBase = elBase;
-        (after as any).elBase = elBase;
-        (inn as any).elBase = elBase;
-
-        const cssItens = `width:18px; height:18px; border-radius:50%;box-shadow: 0 0 4px 1px rgba(57,76,96,.15), 0 0 0 1px rgba(43,59,74,.3); background:#fff; display:flex;justify-content:center; align-items: center; cursor:pointer `
-
-        before.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>`;
-        before.title = 'Before';
-        before.style.cssText = cssItens;
-
-        after.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>`;
-        after.title = 'after';
-        after.style.cssText = cssItens;
-
-        inn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M32 64C14.3 64 0 49.7 0 32S14.3 0 32 0l96 0c53 0 96 43 96 96l0 306.7 73.4-73.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-128 128c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 402.7 160 96c0-17.7-14.3-32-32-32L32 64z"/></svg>`;
-        inn.title = 'in';
-        inn.style.cssText = cssItens;
-
-        if (valid.before) content.appendChild(before);
-        if (valid.after) content.appendChild(after);
-        if (valid.inside) content.appendChild(inn);
-
-        elBase.style.position = 'relative';
-
-        this.myParent.updateSize(elBase, content, true);
-
-        elBase.appendChild(content);
-
-    }
-
-    private changeStateDrop(elBase: IcaLitElementBaseMethods): void {
-
-        if (!elBase) return;
-        if (elBase.getAttribute('renderType') === 'editactive') return;
-
-        elBase.style.position = '';
-        const content = elBase.querySelector(':scope > wcd-dragdrop-aux');
-        if (!content) return;
-        content.remove();
-
-
-    }
-
-}
-
-export const getTemplate = (mode: string = '', position: string = ''): IActionsToolbox => {
-
-    let ret: IActionsToolbox = templateActionMove.move as IActionsToolbox;
-    if (position !== '') ret.position = position as any;
-
-    return ret as IActionsToolbox;
-
-}
-
-const templateActionMove = {
-    move: {
-        position: 'p-m2',
-        tp: 'action',
-        format: '',
-        title: 'Move',
-        iconSvg: '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M278.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l9.4-9.4V224H109.3l9.4-9.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-9.4-9.4H224V402.7l-9.4-9.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-9.4 9.4V288H402.7l-9.4 9.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3l-64-64c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l9.4 9.4H288V109.3l9.4 9.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-64-64z"/></svg>',
-        onclick: undefined,
-        menuItens: [],
-        menuSubItens: [],
-        widget: 'wcd-toolbox-item-action-move-100554',
-        cursor: 'pointer',
-        attrs: undefined,
-        isDblClick: false
-    }
 }
