e4fb209e2f8a51aa76bdd36992688c3e43934774
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-04-16 17:16:46 -0300

    adjust service
    adjust service

---
+++ l2/collabTasks.ts
@@ -0,0 +1,148 @@
+/// <mls shortName="collabTasks" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+
+import { html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { IcaLitElement } from './_100554_icaLitElement';
+import { collab_plus, collab_message } from './_100554_collabIcons';
+
+@customElement('collab-tasks-100554')
+export class CollabTasks100554 extends IcaLitElement {
+
+    render() {
+        return html`<div class="task-container">
+
+         <ol class="task-columns">
+            <li class="task-column">
+                <div class="task-column-container"> 
+                <div class="task-column-title">Pending</div>
+                    <ul class="task-items">
+                        <li class="task-item">  
+                            <div class="task-item-title"> Task 1</div>
+                            <div class="task-item-body"></div>
+                            <div class="task-item-actions">
+                            <div class="task-item-actions-info">
+                                <div class="info-messages"> ${collab_message} <span>10</span></div>
+                                </div>
+                                <div class="task-item-actions-user">
+                                    <img class="avatar" src="https://lh3.googleusercontent.com/a-/AOh14GjhEPN7UazL97l6qFIRIYUoLY-PNNPC93Zw4EVT=s96-c" alt="user avatar">
+                                </div>
+                            </div>
+                        </li>
+                        <li class="task-item">  
+                            <div class="task-item-title"> Task 2</div>
+                            <div class="task-item-body"></div>
+                            <div class="task-item-actions">
+                                <div class="task-item-actions-info"></div>
+                                <div class="task-item-actions-user">
+                                    <img class="avatar" src="https://avatars.githubusercontent.com/u/57486730?v=4" alt="user avatar">
+                                </div>
+                            </div>
+                        </li>
+                        <li class="task-item">  
+                            <div class="task-item-title"> Task 3</div>
+                            <div class="task-item-body"></div>
+                            <div class="task-item-actions">
+                                <div class="task-item-actions-info"></div>
+                                <div class="task-item-actions-user">
+                                    <img class="avatar" src="https://avatars.githubusercontent.com/u/57486730?v=4" alt="user avatar">
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="task-items-add">
+                        ${collab_plus}
+                        <span>Add new task</span>
+                    </div>
+                </div>
+            
+            </li>
+
+            <li class="task-column">
+                <div class="task-column-container"> 
+                <div class="task-column-title">In progress</div>
+                    <ul class="task-items">
+                        <li class="task-item">  
+                            <div class="task-item-title"> Task 4</div>
+                            <div class="task-item-body"></div>
+                            <div class="task-item-actions">
+                                <div class="task-item-actions-info">
+                                    <div class="info-messages"> ${collab_message} <span>1</span></div>
+                                </div>
+                                <div class="task-item-actions-user">
+                                    <img class="avatar" src="https://avatars.githubusercontent.com/u/36516904?v=4" alt="user avatar">
+                                </div>
+                            </div>
+                        </li>
+                        <li class="task-item">  
+                            <div class="task-item-title"> Task 5</div>
+                            <div class="task-item-body"></div>
+                            <div class="task-item-actions">
+                                <div class="task-item-actions-info"></div>
+                                <div class="task-item-actions-user">
+                                    <img class="avatar" src="https://lh3.googleusercontent.com/a-/AOh14GjhEPN7UazL97l6qFIRIYUoLY-PNNPC93Zw4EVT=s96-c" alt="user avatar">
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="task-items-add">
+                        ${collab_plus}
+                        <span>Add new task</span>
+                    </div>
+                </div>
+            </li>
+
+            <li class="task-column">
+                <div class="task-column-container"> 
+                <div class="task-column-title">Completed</div>
+                    <ul class="task-items">
+                        <li class="task-item">  
+                            <div class="task-item-title"> Task 6</div>
+                            <div class="task-item-body"></div>
+                            <div class="task-item-actions">
+                                <div class="task-item-actions-info">
+                                    <div class="info-messages"> ${collab_message} <span>1</span></div>
+                                </div>
+                                <div class="task-item-actions-user">
+                                    <img class="avatar" src="https://avatars.githubusercontent.com/u/36516904?v=4" alt="user avatar">
+                                </div>
+                            </div>
+                        </li>
+                        <li class="task-item">  
+                            <div class="task-item-title"> Task 7</div>
+                            <div class="task-item-body"></div>
+                            <div class="task-item-actions">
+                                <div class="task-item-actions-info">
+                                    <div class="info-messages"> ${collab_message} <span>3</span></div>
+                                </div>
+                                <div class="task-item-actions-user">
+                                    <img class="avatar" src="https://lh3.googleusercontent.com/a-/AOh14GjhEPN7UazL97l6qFIRIYUoLY-PNNPC93Zw4EVT=s96-c" alt="user avatar">
+                                </div>
+                            </div>
+                        </li>
+                        <li class="task-item">  
+                            <div class="task-item-title"> Task 8</div>
+                            <div class="task-item-body"></div>
+                            <div class="task-item-actions">
+                                <div class="task-item-actions-info">
+                                    <div class="info-messages"> ${collab_message} <span>1</span></div>
+                                </div>
+                                <div class="task-item-actions-user">
+                                    <img class="avatar" src="https://lh3.googleusercontent.com/a-/AOh14GjhEPN7UazL97l6qFIRIYUoLY-PNNPC93Zw4EVT=s96-c" alt="user avatar">
+                                </div>
+
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="task-items-add">
+                        ${collab_plus}
+                        <span>Add new task</span>
+                    </div>
+                </div>
+            </li>
+        
+
+         </ol>
+
+         </div>`;
+    }
+}
