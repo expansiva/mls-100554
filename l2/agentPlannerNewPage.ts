b26d8791f7a7e23bb15275ace8e722d5ac194f51
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-04-29 17:32:56 -0300

    remove consoles
    remove consoles

--- l2/agentPlannerNewPage.ts
+++ l2/agentPlannerNewPage.ts
@@ -115,7 +115,7 @@ const _afterClarification = async (context: mls.msg.ExecutionContext, stepId: nu
     nextSteps: null,
     rags: null,
     type: 'agent',
-    title: 'Executing',
+    title: 'Executing', // necessario na api, porem nao existente na interface
   } as any
 
   await addNewStep(context, step.stepId, [newStep]);
