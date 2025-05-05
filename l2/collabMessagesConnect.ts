869cc8b51d4c3ce538eb44662d0d1e53a675abc0
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-05-05 17:47:57 -0300

    adjust css
    adjust css

--- l2/collabMessagesConnect.ts
+++ l2/collabMessagesConnect.ts
@@ -17,7 +17,6 @@ import { IChatPreferences } from './_100554_collabMessageHelper';
 import { IcaLitElement } from './_100554_icaLitElement';
 import { CollabMessagesPrompt100554 } from './_100554_collabMessagesPrompt';
 
-
 /// **collab_i18n_start** 
 const message_pt = {
     loading: 'Carregando...',
