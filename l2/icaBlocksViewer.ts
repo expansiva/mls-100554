2b903a7aa1f63c0837173359328851ed7b0e54fb
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-05-20 15:09:28 -0300

    add new files ICA
    add new files ICA

---
+++ l2/icaBlocksViewer.ts
@@ -0,0 +1,33 @@
+/// <mls shortName="icaBlocksViewer" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+
+import { customElement } from 'lit/decorators.js';
+import { ActionTag, IAllowCommand } from './_100554_icaTypes';
+import { IcaLitElementBase } from './_100554_icaLitElementBase';
+
+export abstract class IcaBlocksViewer extends IcaLitElementBase {
+
+    public mySymbol = 'fa-table-columns';
+
+    public getActionsTags(): ActionTag[] {
+        return [
+            { name: "margin" },
+            { name: "padding" },
+            { name: "menu" },
+            { name: "size" },
+            { name: "title" },
+        ]
+    }
+
+    public changeStateHtml(html: string): void {
+
+    }
+
+    public allowCommand(cmd: string, scope: HTMLElement, target: HTMLElement): IAllowCommand {
+        return { inside: false, before: false, after: false };
+    }
+
+    private commandMove(scope: HTMLElement, target: HTMLElement): IAllowCommand {
+        return { inside: false, before: false, after: false };
+    }
+
+}
