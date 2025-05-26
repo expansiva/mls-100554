d464209d7f4786e3c8f526c55030a27f30884286
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-05-26 17:09:40 -0300

    Alteração ica description e build com .map
    Alteração ica description e build com .map

--- l2/servicePreview.ts
+++ l2/servicePreview.ts
@@ -9,7 +9,7 @@ import { getConfigProject } from './_100554_libProjectConfig';
 import { globalState, setState } from './_100554_collabState';
 import { convertTagToFileName } from './_100554_utilsLit';
 import { collab_record, collab_trash, collab_file_pen, collab_play, collab_test, collab_xmark } from './_100554_collabIcons';
-import { getScriptTest } from './_100554_libProcessTest';
+import { CollabState } from './_100554_collabState';
 import { TsTestAst } from './_100554_tsTestAST';
 import './_100554_collabConsole';
 import './_100554_collabResultTest';
@@ -611,7 +611,7 @@ export class ServicePreview100554 extends ServiceBase {
             const iframe = window.preview.iframe;
             if (!iframe || !iframe.contentWindow || !(iframe.contentWindow as any).globalStateManagment) return;
             const ica = (iframe.contentWindow as any).globalStateManagment
-            const script = getScriptTest(ica);
+            const script = this.getScriptTest(ica);
 
             this.fireEventsDetails(script);
         }
@@ -1031,8 +1031,152 @@ export class ServicePreview100554 extends ServiceBase {
         return elements;
     }
 
+
+    getScriptTest(ica: CollabState): { func: string, exe: any } | undefined {
+
+        const stateHistories = ica.getHistory();
+        if (stateHistories.length <= 0) return undefined;
+
+        const params: Record<string, IParams> = {};
+        const keysCont: Record<string, number> = {};
+
+        let lastParam = '';
+
+        stateHistories.forEach((stateHistory, index) => {
+
+            const paramKey = stateHistory.key.split('.').pop();
+            const paramType = typeof stateHistory.value;
+            const paramValue = this.processValue(stateHistory.value);
+
+            if (!paramKey) return;
+            if (stateHistory.system) {
+                lastParam = paramKey;
+                return;
+            }
+
+            let resultKey = paramKey;
+            if (!keysCont[paramKey]) keysCont[paramKey] = 0;
+
+            if (params[paramKey] && lastParam && lastParam !== resultKey) {
+                keysCont[paramKey] += 1;
+                resultKey = `${paramKey}_${keysCont[paramKey]}`;
+            }
+
+            params[resultKey] = {
+                paramValue,
+                paramType,
+                paramOri: stateHistory.key,
+                history: stateHistory
+            };
+
+
+            (stateHistory as any).paramKey = resultKey;
+            lastParam = resultKey;
+
+        });
+
+
+        const lines: string[] = [];
+
+        let lastPath = '';
+        let lastKey = '';
+
+        let lastInteraction = '';
+        let lastIndex = -1;
+
+
+        stateHistories.forEach((history) => {
+
+            const interationType = history.system ? "System" : "User";
+            const vl = this.processValue(history.value);
+            let row = '';
+
+            if (!history.system) {
+
+                const param = this.getParam(params, (history as any).paramKey);
+                if (!param) return;
+                row = `setState('${history.key}', args.${param})`;
+
+            } else {
+
+                row = `verifyState('${history.key}', ${vl})`;
+                if (typeof vl === "string") row = `verifyState('${history.key}', '${vl}')`;
+            }
+
+            if (lastInteraction === interationType && lastPath === history.key && lastKey === (history as any).paramKey && lastIndex >= 0) {
+
+                lines[lastIndex] = row;
+
+            } else {
+                lines.push(row);
+                lastInteraction = interationType;
+                lastPath = history.key;
+                lastKey = (history as any).paramKey;
+                lastIndex = lines.length - 1;
+            }
+
+        });
+
+        const exe: any = {
+            functionName: '',
+            description: '',
+            page: '',
+            enabled: true,
+            schema: {}
+        };
+
+        Object.keys(params).forEach((k) => {
+
+            const p = params[k];
+            if (!p) return;
+            exe.schema[k] = { type: p.paramType, value: p.paramValue };
+
+        });
+
+        let name = '';
+        if (mls.actual[2]) name = (mls.actual[2] as any).left.shortName;
+        if (name !== '') name = `watchState('[pathTo].labelError', '[Expected Value]');`;
+
+        const func = `export function @funcname(args: Record<string, any>): string {\n${name}\n${lines.join(';\n')}\nreturn 'ok';\n}`
+
+        return { func, exe };
+
+    }
+
+    getParam(params: any, key: string) {
+        const keys = Object.keys(params);
+        let ret = '';
+        keys.forEach((i) => {
+            if (i !== key) return;
+            ret = i;
+        });
+        return ret;
+    }
+
+    processValue(vl: any): string | number {
+        if (typeof vl === "string" || typeof vl === "number") {
+            return vl;
+        }
+        return JSON.stringify(vl);
+    }
+
+
+
+
 }
 
+
+interface IParams {
+    paramValue: string | number,
+    paramType: string,
+    paramOri: string,
+    history: {
+        timestamp: number;
+        system: boolean;
+        key: string;
+        value: any;
+    }
+}
 
 enum ERecord {
     "Stop" = 0,
