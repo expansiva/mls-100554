6320caff71924da661cc78eff43458ea608d762b
Wagner <WagnerExpansiva@users.noreply.github.com>
2025-06-04 17:29:48 -0300

    fix
    fix

--- l2/beHelper.ts
+++ l2/beHelper.ts
@@ -1,6 +1,6 @@
 /// <mls shortName="beHelper" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
 
-import { DatabaseClient, ReadDriver, EntityReadAction, IFieldTable, ITypeFieldDB } from "./_100554_beDatabaseBase";
+import { DatabaseClient, IFieldTable, ITypeFieldDB } from "./_100554_beDatabaseBase";
 
 export function prismaToFieldTable(prisma: string): IFieldTable[] {
   const lines = prisma.split("\n").map(l => l.trim()).filter(l => l && !l.startsWith("model") && !l.startsWith("}"));
