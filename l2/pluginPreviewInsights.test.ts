ecc8bbd97a16badcb174fcf2055b0577295885ac
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-06-26 11:14:54 -0300

    implements previewInsights v1
    implements previewInsights v1

---
+++ l2/pluginPreviewInsights.test.ts
@@ -0,0 +1,6 @@
+/// <mls shortName="pluginPreviewInsights" project="100554" enhancement="_blank" />
+				
+import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST'; 
+
+export const integrations: ICANIntegration[] = []; 
+export const tests: ICANTest[] = [];
