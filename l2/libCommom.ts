b292516b6af24c09f7fc8aef81b4bb89c00ade9b
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-07-11 10:19:57 -0300

    Service Save
    Service Save

--- l2/libCommom.ts
+++ l2/libCommom.ts
@@ -334,9 +334,9 @@ export function calculateTotalStringSize(source: string, limitBase:number):ICalc
 }
 
 function formatSize(bytes: number) {
-    if (bytes < 1024) return `${bytes} B`;
-    if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(2)} KB`;
-    return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+    if (bytes < 1024) return `${bytes}B`;
+    if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(2)}KB`;
+    return `${(bytes / (1024 * 1024)).toFixed(2)}MB`;
 }
 
 interface ICalculateTotalStringSize {
