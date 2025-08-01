43b4ceb36ba0ccb9228921261934055f6d68bd2f
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-08-01 14:30:09 -0300

    adjust collabMessages
    adjust collabMessages

--- l2/collabMessageHelper.ts
+++ l2/collabMessageHelper.ts
@@ -4,9 +4,46 @@ import { getTemporaryContext, getUserIdLocalStorage, notifyMessageSendChange } f
 import { IAgent } from './_100554_aiAgentBase';
 import { collabImport } from './_100554_collabImport';
 
-const LS_KEY = 'collabChatPreferences';
+const LS_KEY_OLD = 'collabChatPreferences';
+const LOCAL_STORAGE_KEY = '_100554_serviceCollabMessages';
 export const AGENTDEFAULT = 'agentPlanner1';
 export const PROJECTAGENTDEFAULT = 100554;
+
+export async function registerToken() {
+
+    const token = await mls.events.getFCMTokenForBackend();
+    if (token === null) {
+        saveNotificationPreferences('denied');
+        return token;
+    }
+
+    const lastToken = loadNotificationToken();
+    if (lastToken === token) return token;
+    
+    saveNotificationToken(token);
+
+    try {
+        const deviceId = crypto.randomUUID();
+        saveNotificationDeviceId(deviceId);
+        console.info({ deviceId })
+
+        const userResponse = await mls.api.msgGetUserUpdate({ userId: "" });
+        await mls.api.msgUpdateUserDetails({
+            userId: userResponse.user.userId,
+            avatar_url: userResponse.user.avatar_url,
+            name: userResponse.user.name,
+            status: userResponse.user.status,
+            deviceId,
+            notificationToken: token
+        });
+
+        saveNotificationPreferences('granted');
+        return token;
+    } catch (err: any) {
+        throw new Error('Error on register token' + err.message);
+    }
+
+}
 
 export async function addMessage(threadId: string, messageContent: string, contextToBot?: mls.bots.ToolsBeforeSendMessage) {
 
@@ -36,7 +73,7 @@ export async function addMessage(threadId: string, messageContent: string, conte
 
 export async function getArgsToBots(): Promise<Record<string, any>> {
     const data = {
-        project: mls.actual[5].project
+        project: mls.actualProject
     }
     return data
 }
@@ -56,11 +93,11 @@ export async function getBotsContext(thread: mls.msg.Thread, prompt: string, con
                 const argsBot: Record<string, any> = await agent.beforeBot(context, prompt, botsVarsBefore2)
                 auxContextToBot.push(argsBot);
             }
-        } catch (err:any) {
+        } catch (err: any) {
             console.error(err.message);
             continue;
         }
-        
+
     }
 
     const merged = auxContextToBot.reduce((acc, curr) => {
@@ -70,20 +107,104 @@ export async function getBotsContext(thread: mls.msg.Thread, prompt: string, con
     return merged;
 }
 
+export function saveNotificationDeviceId(deviceId: string) {
+    let dataLocal: CollabMessagesLS | undefined = loadLocalStorage();
+    if (!dataLocal) dataLocal = { deviceId }
+    else dataLocal.deviceId = deviceId;
+    saveLocalStorage(dataLocal);
+}
+
+export function loadNotificationDeviceId(): string | null {
+    const lsData = loadLocalStorage();
+    if (lsData && lsData.deviceId) return lsData.deviceId;
+    return null;
+}
+
+export function saveNotificationToken(tokenFCM: string) {
+    let dataLocal: CollabMessagesLS | undefined = loadLocalStorage();
+    if (!dataLocal) dataLocal = { tokenFCM }
+    else dataLocal.tokenFCM = tokenFCM;
+    saveLocalStorage(dataLocal);
+}
+
+export function loadNotificationToken(): string | null {
+    const lsData = loadLocalStorage();
+    if (lsData && lsData.tokenFCM) return lsData.tokenFCM;
+    return null;
+}
+
+export function saveNotificationPreferences(notificationPreference: NotificationPermission) {
+    let dataLocal: CollabMessagesLS | undefined = loadLocalStorage();
+    if (!dataLocal) dataLocal = { notificationPreference }
+    else dataLocal.notificationPreference = notificationPreference;
+    saveLocalStorage(dataLocal);
+}
+
+export function loadNotificationPreferences(): NotificationPermission | null {
+    const lsData = loadLocalStorage();
+    if (lsData && lsData.notificationPreference) return lsData.notificationPreference;
+    return null;
+}
+
+export function saveLastTab(lastTab: string) {
+    let dataLocal: CollabMessagesLS | undefined = loadLocalStorage();
+    if (!dataLocal) dataLocal = { lastTab }
+    else dataLocal.lastTab = lastTab;
+    saveLocalStorage(dataLocal);
+}
+
+export function loadLastTab(): string {
+    const lsData = loadLocalStorage();
+    if (lsData && lsData.lastTab) return lsData.lastTab;
+    return 'CRM';
+}
+
 export function loadChatPreferences(): IChatPreferences {
-    const saved = localStorage.getItem(LS_KEY);
-    if (saved) {
+    const savedOld = localStorage.getItem(LS_KEY_OLD);
+    if (savedOld) {
         try {
-            return JSON.parse(saved);
+            const data = JSON.parse(savedOld);
+            saveChatPreferences(data);
+            localStorage.removeItem(LS_KEY_OLD);
         } catch (e) {
+            localStorage.removeItem(LS_KEY_OLD);
             console.warn('Invalid preferences in localStorage');
         }
     }
+
+    const lsData = loadLocalStorage();
+    if (lsData && lsData.chatPreferences) return lsData.chatPreferences;
     return loadDefaultPreferences();
 }
 
 export function saveChatPreferences(chatPreferences: IChatPreferences) {
-    localStorage.setItem(LS_KEY, JSON.stringify(chatPreferences));
+    let dataLocal: CollabMessagesLS | undefined = loadLocalStorage();
+    if (!dataLocal) {
+        dataLocal = { chatPreferences }
+    } else {
+        dataLocal.chatPreferences = chatPreferences;
+    }
+    saveLocalStorage(dataLocal);
+}
+
+function saveLocalStorage(data: CollabMessagesLS) {
+    try {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
+    } catch (e) {
+        console.error('Erro ao salvar no localStorage:', e);
+    }
+}
+
+function loadLocalStorage() {
+
+    let dataLocal: CollabMessagesLS | undefined;
+    try {
+        const stored = localStorage.getItem(LOCAL_STORAGE_KEY);
+        if (stored) dataLocal = JSON.parse(stored);
+        return dataLocal;
+    } catch (e) {
+        console.error('Erro ao carregar do localStorage:', e);
+    }
 }
 
 function loadDefaultPreferences(): IChatPreferences {
@@ -111,3 +232,13 @@ export interface IChatPreferences {
     language: string,
     threadMaintenance: string
 }
+
+export interface CollabMessagesLS {
+    lastTab?: string,
+    chatPreferences?: IChatPreferences,
+    tokenFCM?: string,
+    deviceId?: string,
+    notificationPreference?: NotificationPermission
+}
+
+
