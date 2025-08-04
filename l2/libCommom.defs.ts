7dfc30ff3ff209f7cd2f7b23123abf3bdab928c8
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-08-04 10:10:26 -0300

    implements file projects and adjust collabInit
    implements file projects and adjust collabInit

---
+++ l2/libCommom.defs.ts
@@ -0,0 +1,2 @@
+/// <mls shortName="libCommom" project="100554" enhancement="_blank" folder="" />
+
---
+++ l2/projects.defs.ts
@@ -0,0 +1,2 @@
+/// <mls shortName="projects" project="100554" enhancement="_blank" folder="" />
+
