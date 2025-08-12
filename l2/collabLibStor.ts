41f68c3acb17be06c405b256abcac0c0c6c96023
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-08-12 09:54:34 -0300

    CollabLibStor  e collablibimport
    CollabLibStor  e collablibimport

--- l2/collabLibStor.ts
+++ l2/collabLibStor.ts
@@ -278,7 +278,7 @@ export async function undoAllFiles(storFile: mls.stor.IFileInfo): Promise<void> 
     }
 
     await mls.stor.localDB.removePrjInfo(storFile.project);
-
+    createAllModels(storFile)
 }
 
 function isNewNameValid(newShortName: string): boolean {
