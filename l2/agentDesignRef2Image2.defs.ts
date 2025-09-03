483fd37ff31f01279477e251864710a1c9417e65
Wagner <WagnerExpansiva@users.noreply.github.com>
2025-09-03 17:29:12 -0300

    agent images
    agent images

---
+++ l2/agentDesignRef2Image2.defs.ts
@@ -0,0 +1,2 @@
+/// <mls shortName="agentDesignRef2Image2" project="100554" enhancement="_blank" folder="" />
+
---
+++ l2/agentImage2DesignRef.defs.ts
@@ -0,0 +1,2 @@
+/// <mls shortName="agentImage2DesignRef" project="100554" enhancement="_blank" folder="" />
+
