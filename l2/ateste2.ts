fcfc701d6e674cbde5954a72d559373195746117
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-09-04 09:35:47 -0300

    Modo SinglePage
    Modo SinglePage

--- l2/ateste2.ts
+++ l2/ateste2.ts
@@ -2,7 +2,10 @@
 
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { CollabLitElement } from './_100554_collabLitElement'
+import { CollabLitElement } from './_100554_collabLitElement';
+//import { CollabLitElement } from './_100554_/l2/collabLitElement';
+//import { CollabLitElement } from './l2/collabLitElement'
+// colocar no console: mls.modePreview = 'minimum'
 
 const message_pt = {
     hello: 'Hello world!'
@@ -11,16 +14,12 @@ const message_pt = {
 @customElement('ateste2-100554')
 export class SimpleGreeting extends CollabLitElement {
 
-    @property() name: string = new Date(Date.now()).toString();
+    @property() name: string = 'Roberto';
 
     handleConfirm(e: CustomEvent) {
         console.info(e.detail)
     }
     
-    showGreetingAlert() {
-        alert(`Hello world Lucas 10`);
-    } 
-
     render() {
         return html`<div class="cls1">
             Meu nome é: ${this.name}
