249fde247cb55c080a0dea35dca89e50cf2abd12
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-10-29 08:25:14 -0300

    teste l1
    teste l1

---
+++ l1/ateste.ts
@@ -0,0 +1,8 @@
+/// <mls shortName="ateste" project="100554" folder="" enhancement="_blank" groupName="" />
+
+import {teste} from './_100554_bteste'; 
+import {convertFileNameToTag} from './_100554_utilsLit';
+
+
+
+console.info(teste()); 
---
+++ l1/bteste.ts
@@ -0,0 +1,5 @@
+/// <mls shortName="bteste" project="100554" folder="" enhancement="_blank" groupName="" />
+
+export function teste(){
+    console.info('estou no bteste');
+}
