d2e83d2e11d8360edb981bfedc99b4bfaaaebdd5
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-11-07 10:42:03 -0300

    adjust project
    adjust project

--- l2/project.ts
+++ l2/project.ts
@@ -1,3 +1,5 @@
 /// <mls shortName="project" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
 
-export const modules = [];
+export const projectConfig = {
+    modules: []
+}
