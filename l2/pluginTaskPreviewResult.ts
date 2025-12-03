44682437ad3480b2f43d60b5f3578390ff681f23
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-03 10:24:35 -0300

    refactor imports v1

--- l2/pluginTaskPreviewResult.ts
+++ l2/pluginTaskPreviewResult.ts
@@ -2,7 +2,7 @@
 
 import { html, repeat } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
-import { CollabLitElement } from './_100554_collabLitElement';  
+import { CollabLitElement } from '/_100554_/l2/collabLitElement.js';  
   
 @customElement('plugin-task-preview-result-100554')
 export class pluginTaskPreviewResult extends CollabLitElement {
