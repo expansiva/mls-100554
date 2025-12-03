63b5451aba65330354c77b302a0a043ef71e0de6
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-03 15:45:16 -0300

    refactor imports v4

--- l2/icaTemplateBase.ts
+++ l2/icaTemplateBase.ts
@@ -1,6 +1,6 @@
 /// <mls shortName="icaTemplateBase" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
 
-import { StateLitElement } from './_100554_stateLitElement';
+import { StateLitElement } from '/_100554_/l2/stateLitElement.js';
 
 export abstract class IcaTemplateBase extends StateLitElement {
 
