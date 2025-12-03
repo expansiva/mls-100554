a36af70fd6e9ebf6a6db43965cd4a24b2cb22e8d
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-03 15:03:24 -0300

    refactor import v2

--- l2/serviceProject.ts
+++ l2/serviceProject.ts
@@ -2,11 +2,11 @@
 
 import { html, css, repeat } from 'lit';
 import { customElement, property, query, queryAll } from 'lit/decorators.js';
-import { ServiceBase, IService, IToolbarContent, IServiceMenu, IOptions } from './_100554_serviceBase';
-import { collab_user } from './_100554_collabIcons';
-import { getAllWebComponentsInSource } from './_100554_libCompile';
-import { convertTagToFileName, convertFileNameToTag } from './_100554_utilsLit';
-import { loadPluginProject } from './_100554_libCommom';
+import { ServiceBase, IService, IToolbarContent, IServiceMenu, IOptions } from '/_100554_/l2/serviceBase.js';
+import { collab_user } from '/_100554_/l2/collabIcons.js';
+import { getAllWebComponentsInSource } from '/_100554_/l2/libCompile.js';
+import { convertTagToFileName, convertFileNameToTag } from '/_100554_/l2/utilsLit.js';
+import { loadPluginProject } from '/_100554_/l2/libCommom.js';
 import('./_100554_collabPanel');
 
 /// **collab_i18n_start**
