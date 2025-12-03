2afbc6d5473d866589ede236275fa98a150c4b91
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-03 10:20:43 -0300

    refactor imports v1

--- l2/collabL3PreviewTextI18n.ts
+++ l2/collabL3PreviewTextI18n.ts
@@ -2,8 +2,8 @@
 
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { propertyDataSource } from './_100554_collabDecorators';
-import { StateLitElement } from './_100554_stateLitElement';
+import { propertyDataSource } from '/_100554_/l2/collabDecorators.js';
+import { StateLitElement } from '/_100554_/l2/stateLitElement.js';
 
 @customElement('collab-l3-preview-text-i18n-100554')
 export class CollabL3PreviewTextI18n extends StateLitElement {
