fda4c6636892a347743d9aa0b2ff39dc188ec084
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-03 15:47:07 -0300

    refactor imports v4

--- l2/wcdOverlayModeDefaultHelp.ts
+++ l2/wcdOverlayModeDefaultHelp.ts
@@ -1,6 +1,6 @@
 /// <mls shortName="wcdOverlayModeDefaultHelp" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
 
-import { CollabPageElement } from './_100554_collabPageElement'
+import { CollabPageElement } from '/_100554_/l2/collabPageElement.js'
 import { customElement } from 'lit/decorators.js';
 
 @customElement('wcd-overlay-mode-default-help-100554')
