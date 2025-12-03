7006a0e52a3e8c200093a9279430f9e5569c8369
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-03 15:25:31 -0300

    refactor import v3

--- l2/pluginPageAIVerify.ts
+++ l2/pluginPageAIVerify.ts
@@ -2,7 +2,7 @@
 
 import { html, css, svg, repeat, TemplateResult } from 'lit';
 import { property, queryAll } from 'lit/decorators.js';
-import { PluginBaseModule } from './_100554_pluginBaseModule';
+import { PluginBaseModule } from '/_100554_/l2/pluginBaseModule.js'; 
 
 /// **collab_i18n_start**
 const message_pt = {
