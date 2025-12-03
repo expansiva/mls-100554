3410c3b9ce158690e02d537f66e4f3edd0ea62b6
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-03 10:22:18 -0300

    refactor imports v1

--- l2/collabTasks.ts
+++ l2/collabTasks.ts
@@ -2,8 +2,8 @@
 
 import { html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
-import { StateLitElement } from './_100554_stateLitElement';
-import { collab_spinner_clock } from './_100554_collabIcons';
+import { StateLitElement } from '/_100554_/l2/stateLitElement.js';
+import { collab_spinner_clock } from '/_100554_/l2/collabIcons.js';
 
 @customElement('collab-tasks-100554')
 export class CollabTasks100554 extends StateLitElement {
