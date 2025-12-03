eeed57bc8d19e4152b96b5a22e09788795bb99ed
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-03 15:24:32 -0300

    refactor import v3

---
+++ l2/pluginBase.test.ts
@@ -0,0 +1,5 @@
+/// <mls shortName="pluginBase" project="100554" enhancement="_blank" folder="" />
+
+ import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
+ export const integrations: ICANIntegration[] = [];
+ export const tests: ICANTest[] = [];
