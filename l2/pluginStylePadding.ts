f9e2fe95b48bd1021937978aa79cd9691b6f65e4
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-03 15:26:37 -0300

    refactor import v3

--- l2/pluginStylePadding.ts
+++ l2/pluginStylePadding.ts
@@ -2,13 +2,13 @@
 
 import { html, repeat } from 'lit';
 import { customElement, property, query, queryAll } from 'lit/decorators.js';
-import { StateLitElement } from './_100554_stateLitElement';
-import { propertyDataSource } from './_100554_collabDecorators';
-import { getState, setState } from './_100554_collabState';
-import { getMessageKey } from './_100554_collabLitElement';
-import { ICSSState } from './_100554_lessCSS';
-import './_100554_collabDsInputSelectColor';
-import './_100554_collabDsInputRange';
+import { StateLitElement } from '/_100554_/l2/stateLitElement.js';
+import { propertyDataSource } from '/_100554_/l2/collabDecorators.js';
+import { getState, setState } from '/_100554_/l2/collabState.js';
+import { getMessageKey } from '/_100554_/l2/collabLitElement.js';
+import { ICSSState } from '/_100554_/l2/lessCSS.js';
+import '/_100554_/l2/collabDsInputSelectColor.js';
+import '/_100554_/l2/collabDsInputRange.js';
 import {
     collab_lock,
     collab_lock_open,
@@ -18,7 +18,7 @@ import {
     collab_padding_right,
 
 
-} from './_100554_collabIcons'
+} from '/_100554_/l2/collabIcons.js'
 
 /// **collab_i18n_start**
 const message_pt = {
