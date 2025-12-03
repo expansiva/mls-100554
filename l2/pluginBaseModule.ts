9703072bc3f410de16cfc78a46dda5dd9c938356
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-03 15:24:37 -0300

    refactor import v3

--- l2/pluginBaseModule.ts
+++ l2/pluginBaseModule.ts
@@ -1,7 +1,7 @@
 /// <mls shortName="pluginBaseModule" project="100554" enhancement="_100554_enhancementLit" />
 
 import { property } from 'lit/decorators.js';
-import { StateLitElement } from '/_100554_/l2/stateLitElement';
+import { StateLitElement } from '/_100554_/l2/stateLitElement.js';
 
 export abstract class PluginBaseModule extends StateLitElement {
 
