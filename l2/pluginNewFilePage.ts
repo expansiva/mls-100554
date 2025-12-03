ebe566aab0ed323ccff12ed51a2453f958ccf47d
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-03 15:25:22 -0300

    refactor import v3

--- l2/pluginNewFilePage.ts
+++ l2/pluginNewFilePage.ts
@@ -2,13 +2,13 @@
 
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { convertFileNameToTag } from './_100554_utilsLit';
-import { StateLitElement } from './_100554_stateLitElement';
-import { getMessageKey } from "./_100554_collabLitElement";
-import { propertyDataSource } from './_100554_collabDecorators';
-import { IDetails, createNewFile, changeTagName, changeClassName, changeWidget, changeStateName, getTemplateImport } from "./_100554_pluginNewFileBase";
-import { ServiceBase } from './_100554_serviceBase';
-import './_100554_widgetTextCode';
+import { convertFileNameToTag } from '/_100554_/l2/utilsLit.js';
+import { StateLitElement } from '/_100554_/l2/stateLitElement.js';
+import { getMessageKey } from "/_100554_/l2/collabLitElement.js";
+import { propertyDataSource } from '/_100554_/l2/collabDecorators.js';
+import { IDetails, createNewFile, changeTagName, changeClassName, changeWidget, changeStateName, getTemplateImport } from "/_100554_/l2/pluginNewFileBase.js";
+import { ServiceBase } from '/_100554_/l2/serviceBase.js';
+import '/_100554_/l2/widgetTextCode.js';
 
 /// **collab_i18n_start**
 const message_pt = {
