dfc2524dac74a39f4411d1e1104ec64e4fe5375a
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-12-04 14:55:09 -0300

    adjust collabMessages

--- l2/serviceSourceL1.ts
+++ l2/serviceSourceL1.ts
@@ -8,11 +8,11 @@ import { propertyDataSource } from '/_100554_/l2/collabDecorators.js';
 import {  collab_typescript, collab_file_code } from '/_100554_/l2/collabIcons.js';
 import { createAgent } from '/_100554_/l2/agentFix.js';
 import { getTemporaryContext } from '/_100554_/l2/aiAgentHelper.js';
-import {  getUserId, createThread } from '/_100554_/l2/collabMessageHelper.js';
+import {  getUserId, createThread } from '/_102025_/l2/collabMessagesHelper.js';
 import { saveOpenedFile, getLastOpenedFiles, OpenedFileL2, getBaseTemplate } from '/_100554_/l2/libCommom.js';
 import { createAllModels, readProjectTypescriptAndCompile, createModel } from '/_100554_/l2/collabLibModel.js';
 import { IReqCreateStorFile, createStorFile } from '/_100554_/l2/collabLibStor.js';
-import { getThreadByName } from '/_100554_/l2/msgDBController.js';
+import { getThreadByName } from '/_102025_/l2/collabMessagesIndexedDB.js';
 
 import { CollabSpliterVerticalVarFixed100554 } from '/_100554_/l2/collabSpliterVerticalVarFixed.js';
 import '/_100554_/l2/collabSpliterVerticalVarFixed.js';
