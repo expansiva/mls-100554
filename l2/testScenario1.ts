3d844fd6a608b8eba088b06c22dd0c2d28055358
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-05 10:14:40 -0300

    refactor imports v10

--- l2/testScenario1.ts
+++ l2/testScenario1.ts
@@ -1,6 +1,6 @@
 /// <mls shortName="testScenario1" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
 
-import { IScenaryDetails } from './_100554_collabLitElement';
+import { IScenaryDetails } from '/_100554_/l2/collabLitElement.js'; 
 export function _100554_testScenario1_getScenaryDetails(): IScenaryDetails {
     const html = document.createElement('wc-input-text-100554');
     return {
