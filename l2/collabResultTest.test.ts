49f84c3a929157d44adf8a5f7337c0f8b1982fcb
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-09 10:15:06 -0300

    refactor imports v12 .test.ts

--- l2/collabResultTest.test.ts
+++ l2/collabResultTest.test.ts
@@ -1,6 +1,6 @@
 /// <mls shortName="collabResultTest" project="100554" enhancement="_blank" />
 				
-import { ICANTest, ICANIntegration  } from './_100554_tsTestAST'; 
+import { ICANTest, ICANIntegration  } from '/_100554_/l2/tsTestAST.js'; 
 
 export const integrations: ICANIntegration[] = []; 
 export const tests: ICANTest[] = [];
