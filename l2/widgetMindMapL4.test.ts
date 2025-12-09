d2a96698fec0ebf622e8bfcf488562aeed1e77e2
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-09 10:21:54 -0300

    refactor imports v12 .test.ts

--- l2/widgetMindMapL4.test.ts
+++ l2/widgetMindMapL4.test.ts
@@ -1,6 +1,6 @@
 /// <mls shortName="widgetMindMapL4" project="100554" enhancement="_blank" />
 				
-import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST'; 
+import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js'; 
 
 export const integrations: ICANIntegration[] = []; 
 export const tests: ICANTest[] = [];
