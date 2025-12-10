92a25f57cd7b8b7fbeb36f587e7899da3788df21
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-12-10 12:41:04 -0300

    adjust agents config

--- l2/agentCreateNewPrototypeOrganism2.ts
+++ l2/agentCreateNewPrototypeOrganism2.ts
@@ -30,7 +30,7 @@ export function createAgent(): IAgent {
         avatar_url: svg_agent,
         agentDescription: "Responsavel por fazer o arquivo ts de um novo organismo",
         visibility: "public",
-        scope: ['l2_preview'],
+        scope: [],
         async beforePrompt(context: mls.msg.ExecutionContext): Promise<void> {
             return _beforePrompt(context);
         },
