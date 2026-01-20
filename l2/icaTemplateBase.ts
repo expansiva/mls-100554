5518b228e011edc8159254b056ef3f18c76d7df7
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-01-20 09:12:19 -0300

    delete: l2/icaTemplateBase.ts

--- l2/icaTemplateBase.ts
+++
@@ -1,7 +0,0 @@
-/// <mls shortName="icaTemplateBase" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
-
-import { StateLitElement } from '/_100554_/l2/stateLitElement.js';
-
-export abstract class IcaTemplateBase extends StateLitElement {
-
-}
