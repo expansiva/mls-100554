ebd2463de43a186ad11b77c71d464621bd645905
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2026-01-21 09:05:36 -0300

    save: l2/pluginBaseModule.ts

--- l2/pluginBaseModule.ts
+++ l2/pluginBaseModule.ts
@@ -1,12 +1,12 @@
 /// <mls shortName="pluginBaseModule" project="100554" enhancement="_100554_enhancementLit" />
 
+
 import { property } from 'lit/decorators.js';
 import { StateLitElement } from '/_100554_/l2/stateLitElement.js';
 
 export abstract class PluginBaseModule extends StateLitElement {
 
     @property() scope: 'detail' | 'dashboard' = 'dashboard';
-
     abstract render(): any;
-
+    
 }
