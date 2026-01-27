bd046bfe3252a647a86ee03605a4bd3c65e0f014
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:00:49 -0300

    save: l2/mlsHistoryList.defs.ts

--- l2/mlsHistoryList.defs.ts
+++ l2/mlsHistoryList.defs.ts
@@ -1,2 +1,71 @@
-/// <mls shortName="mlsHistoryList" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/mlsHistoryList.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/mlsHistoryList.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html"
+          },
+          {
+            "name": "unsafeHTML"
+          },
+          {
+            "name": "css"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement"
+          },
+          {
+            "name": "property"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/collabLitElement.js",
+        "dependencies": [
+          {
+            "name": "CollabLitElement",
+            "type": "class"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "MLS History List Component",
+      "businessCapabilities": [
+        "Display file history",
+        "Handle history item selection"
+      ],
+      "technicalCapabilities": [
+        "Lit-based web component",
+        "Async history retrieval",
+        "Event dispatching"
+      ],
+      "implementedFeatures": [
+        "Loading indicator",
+        "Error display",
+        "Date offset calculations",
+        "History grouping and filtering"
+      ]
+    }
+  }
+}
+    
