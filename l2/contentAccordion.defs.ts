fc6156ab3c9c2a88bb30ea519d1d3529827b9ee9
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:00:13 -0300

    save: l2/contentAccordion.defs.ts

--- l2/contentAccordion.defs.ts
+++ l2/contentAccordion.defs.ts
@@ -1,2 +1,63 @@
-/// <mls shortName="contentAccordion" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/contentAccordion.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/contentAccordion.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html"
+          },
+          {
+            "name": "unsafeHTML"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement"
+          },
+          {
+            "name": "property"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/collabLitElement.js",
+        "dependencies": [
+          {
+            "name": "CollabLitElement"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Content Accordion Component",
+      "businessCapabilities": [
+        "Displays collapsible content sections"
+      ],
+      "technicalCapabilities": [
+        "Uses Lit for rendering",
+        "Custom element with properties"
+      ],
+      "implementedFeatures": [
+        "Toggle functionality",
+        "Selected index property"
+      ]
+    }
+  }
+}
+    
