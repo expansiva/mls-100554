92aa7ad547afcafd7365e0da25033e50e0f6729b
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:43:05 -0300

    save: l2/testMindMap.defs.ts

--- l2/testMindMap.defs.ts
+++ l2/testMindMap.defs.ts
@@ -1,2 +1,82 @@
-/// <mls shortName="testMindMap" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/testMindMap.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/testMindMap.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "languages": [
+      "en",
+      "pt"
+    ],
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement",
+            "type": "function"
+          },
+          {
+            "name": "state",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/stateLitElement.js",
+        "dependencies": [
+          {
+            "name": "StateLitElement",
+            "type": "class"
+          }
+        ]
+      }
+    ]
+  },
+  "codeInsights": {
+    "todos": [
+      "Replace static pages with actual logic",
+      "Replace static tables with actual logic"
+    ],
+    "deadCodeBlocks": [
+      "gemini constant"
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Test Mind Map Component",
+      "businessCapabilities": [
+        "Mind mapping for projects",
+        "Mind mapping for organizations"
+      ],
+      "technicalCapabilities": [
+        "Lit web component",
+        "Mind map integration"
+      ],
+      "implementedFeatures": [
+        "Adds base for project",
+        "Adds dynamic for project",
+        "Adds base for projects",
+        "Adds dynamic for projects",
+        "Adds base for organizations",
+        "Adds dynamic for organizations"
+      ]
+    }
+  }
+}
+    
