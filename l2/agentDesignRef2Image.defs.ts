16c14098c29893d7db399aefa40f3ed44103d1d9
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 16:12:28 -0300

    save: l2/agentDesignRef2Image.defs.ts

--- l2/agentDesignRef2Image.defs.ts
+++ l2/agentDesignRef2Image.defs.ts
@@ -1,2 +1,89 @@
-/// <mls shortName="agentDesignRef2Image" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/agentDesignRef2Image.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/agentDesignRef2Image.ts",
+    "componentType": "agent",
+    "componentScope": "editor",
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "/_100554_/l2/aiAgentBase.js",
+        "dependencies": [
+          {
+            "name": "IAgent",
+            "type": "interface"
+          },
+          {
+            "name": "svg_agent",
+            "type": "constant"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/aiPrompts.js",
+        "dependencies": [
+          {
+            "name": "getPromptByHtml",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/aiAgentHelper.js",
+        "dependencies": [
+          {
+            "name": "getNextInProgressStepByAgentName",
+            "type": "function"
+          },
+          {
+            "name": "getAgentStepByAgentName",
+            "type": "function"
+          },
+          {
+            "name": "updateStepStatus",
+            "type": "function"
+          },
+          {
+            "name": "updateTaskTitle",
+            "type": "function"
+          },
+          {
+            "name": "getNextStepIdAvaliable",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/aiAgentOrchestration.js",
+        "dependencies": [
+          {
+            "name": "startNewAiTask",
+            "type": "function"
+          },
+          {
+            "name": "addNewStep",
+            "type": "function"
+          },
+          {
+            "name": "executeNextStep",
+            "type": "function"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "DesignRef 2 Hero Image",
+      "businessCapabilities": [],
+      "technicalCapabilities": [],
+      "implementedFeatures": []
+    }
+  }
+}
+    
