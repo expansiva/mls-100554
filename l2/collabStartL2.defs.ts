4ce7ab22f2c19d5a975d7ee8601f0a760afa1f65
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 16:59:57 -0300

    save: l2/collabStartL2.defs.ts

--- l2/collabStartL2.defs.ts
+++ l2/collabStartL2.defs.ts
@@ -1,2 +1,71 @@
-/// <mls shortName="collabStartL2" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/collabStartL2.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/collabStartL2.ts",
+    "componentType": "organism",
+    "componentScope": "appFrontEnd",
+    "languages": [
+      "en",
+      "pt"
+    ],
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/stateLitElement.js",
+        "dependencies": [
+          {
+            "name": "StateLitElement",
+            "type": "class"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Module L2 - Components: Description and Features",
+      "businessCapabilities": [
+        "Program Visual Components",
+        "Documentation and Description",
+        "Test Across Devices and Screen Sizes",
+        "Include Component Dependencies",
+        "Program Internal Components for Collab.codes"
+      ],
+      "technicalCapabilities": [
+        "Lit web component",
+        "i18n support"
+      ],
+      "implementedFeatures": [
+        "Creation of visual components",
+        "Component documentation",
+        "Testing on devices",
+        "Dependency management",
+        "Internal component development"
+      ]
+    }
+  }
+}
+    
