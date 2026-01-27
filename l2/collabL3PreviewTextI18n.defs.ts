8027e3807ec2c5bb0534fab2f4423db83a2024f1
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 16:13:24 -0300

    save: l2/collabL3PreviewTextI18n.defs.ts

--- l2/collabL3PreviewTextI18n.defs.ts
+++ l2/collabL3PreviewTextI18n.defs.ts
@@ -1,2 +1,76 @@
-/// <mls shortName="collabL3PreviewTextI18n" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/collabL3PreviewTextI18n.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/collabL3PreviewTextI18n.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement"
+          },
+          {
+            "name": "property"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/collabDecorators.js",
+        "dependencies": [
+          {
+            "name": "propertyDataSource"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/stateLitElement.js",
+        "dependencies": [
+          {
+            "name": "StateLitElement"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Lit custom element for previewing text with i18n",
+      "businessCapabilities": [
+        "Handles click events",
+        "Handles change events",
+        "Fires events for editing"
+      ],
+      "technicalCapabilities": [
+        "Uses Lit framework",
+        "Implements contenteditable",
+        "Extends StateLitElement"
+      ],
+      "implementedFeatures": [
+        "@property findby",
+        "@propertyDataSource value",
+        "render method",
+        "handleChange method",
+        "handleClick method",
+        "fireEvent method",
+        "onSelect method"
+      ]
+    }
+  }
+}
+    
