24381d4f8e1bd702bc6d77c9ecac3316ecf1e241
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:00:38 -0300

    save: l2/lessMonaco.defs.ts

--- l2/lessMonaco.defs.ts
+++ l2/lessMonaco.defs.ts
@@ -1,2 +1,28 @@
-/// <mls shortName="lessMonaco" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/lessMonaco.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/lessMonaco.ts",
+    "componentType": "tool",
+    "componentScope": "editor",
+    "group": "enhancement"
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "class to interface with monaco models",
+      "businessCapabilities": [],
+      "technicalCapabilities": [
+        "getLines",
+        "insertLine",
+        "deleteLine",
+        "deleteLines",
+        "updateLine",
+        "finishEdit"
+      ],
+      "implementedFeatures": []
+    }
+  }
+}
+    
