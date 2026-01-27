5a63518d4e10b5b072c6c49e96c0f30e12e8b4cd
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:00:04 -0300

    save: l2/collabState.defs.ts

--- l2/collabState.defs.ts
+++ l2/collabState.defs.ts
@@ -1,2 +1,49 @@
-/// <mls shortName="collabState" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/collabState.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/collabState.ts",
+    "componentType": "tool",
+    "componentScope": "editor",
+    "group": "enhancement"
+  },
+  "references": {
+    "statesRO": [
+      "db.products"
+    ],
+    "statesRW": [],
+    "statesWO": []
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Manages shared state",
+      "businessCapabilities": [
+        "State retrieval",
+        "State updates",
+        "Subscription management"
+      ],
+      "technicalCapabilities": [
+        "getState",
+        "setState",
+        "subscribe",
+        "unsubscribe",
+        "notify",
+        "initState"
+      ],
+      "implementedFeatures": [
+        "getState",
+        "setState",
+        "subscribe",
+        "unsubscribe",
+        "notify",
+        "initState",
+        "getHistory",
+        "clearHistory",
+        "getStateStatistics"
+      ]
+    }
+  }
+}
+    
