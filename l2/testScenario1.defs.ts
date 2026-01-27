a6595d55263783ac9b505170edb251677486989f
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:43:07 -0300

    save: l2/testScenario1.defs.ts

--- l2/testScenario1.defs.ts
+++ l2/testScenario1.defs.ts
@@ -1,2 +1,43 @@
-/// <mls shortName="testScenario1" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/testScenario1.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/testScenario1.ts",
+    "componentType": "other",
+    "componentScope": "appFrontEnd",
+    "group": "enhancement"
+  },
+  "references": {
+    "webComponents": [
+      "wc-input-text-100554"
+    ],
+    "imports": [
+      {
+        "ref": "/_100554_/l2/collabLitElement.js",
+        "dependencies": [
+          {
+            "name": "IScenaryDetails",
+            "type": "interface"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Function to get scenario details",
+      "businessCapabilities": [
+        "Cenário 1"
+      ],
+      "technicalCapabilities": [
+        "Creates HTML element"
+      ],
+      "implementedFeatures": [
+        "Returns description and HTML"
+      ]
+    }
+  }
+}
+    
