863dab5110f29f44ecb9c059872b3444883cd94f
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2026-01-27 14:21:36 -0300

    save: l2/pluginOrganismProperty.defs.ts

--- l2/pluginOrganismProperty.defs.ts
+++ l2/pluginOrganismProperty.defs.ts
@@ -1,2 +1,81 @@
-/// <mls shortName="pluginOrganismProperty" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/pluginOrganismProperty.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/pluginOrganismProperty.ts",
+    "componentType": "organism",
+    "componentScope": "appFrontEnd",
+    "languages": [
+      "en",
+      "pt"
+    ],
+    "group": "enhancement",
+    "devFidelity": "draft"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement",
+            "type": "function"
+          },
+          {
+            "name": "state",
+            "type": "function"
+          },
+          {
+            "name": "query",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/collabLitElement.js",
+        "dependencies": [
+          {
+            "name": "CollabLitElement",
+            "type": "class"
+          }
+        ]
+      }
+    ]
+  },
+  "codeInsights": {
+    "unusedImports": [
+      "state",
+      "query"
+    ],
+    "i18nWarnings": [
+      "Hardcoded string 'In development' should use i18n"
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Plugin organism property component displaying in development message",
+      "businessCapabilities": [],
+      "technicalCapabilities": [
+        "Renders HTML using Lit",
+        "Supports i18n for English and Portuguese"
+      ],
+      "implementedFeatures": [
+        "Basic Lit element with custom element decorator",
+        "i18n setup"
+      ],
+      "constraints": []
+    }
+  }
+}
+    
