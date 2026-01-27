a98199794f5890bb4d22eba1e6f233044f4cb1ef
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:43:14 -0300

    save: l2/tsTestASTTeste.defs.ts

--- l2/tsTestASTTeste.defs.ts
+++ l2/tsTestASTTeste.defs.ts
@@ -1,2 +1,87 @@
-/// <mls shortName="tsTestASTTeste" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/tsTestASTTeste.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/tsTestASTTeste.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html"
+          },
+          {
+            "name": "css"
+          },
+          {
+            "name": "LitElement"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement"
+          },
+          {
+            "name": "property"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/tsTestAST.js",
+        "dependencies": [
+          {
+            "name": "TsTestAst"
+          },
+          {
+            "name": "ICANIntegration"
+          },
+          {
+            "name": "ICANTest"
+          }
+        ]
+      }
+    ]
+  },
+  "codeInsights": {
+    "deadCodeBlocks": [
+      "const testAddTest = ''",
+      "const testAddTestSameTitle = ''",
+      "const testAddIntegration= ''",
+      "const testDeleteTest = ''"
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Test runner component for AST testing",
+      "businessCapabilities": [],
+      "technicalCapabilities": [
+        "parse AST",
+        "get integrations",
+        "get tests",
+        "add test",
+        "add integration",
+        "delete test"
+      ],
+      "implementedFeatures": [
+        "exeTest",
+        "test1",
+        "test2",
+        "test3",
+        "test4",
+        "test5",
+        "test6"
+      ]
+    }
+  }
+}
+    
