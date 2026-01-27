7774e64e7520b8e40cc0049c335a87f14c8f2a97
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:43:24 -0300

    save: l2/widgetDividerLine.defs.ts

--- l2/widgetDividerLine.defs.ts
+++ l2/widgetDividerLine.defs.ts
@@ -1,2 +1,71 @@
-/// <mls shortName="widgetDividerLine" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/widgetDividerLine.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/widgetDividerLine.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "group": "enhancement"
+  },
+  "references": {
+    "webComponents": [
+      "widget-divider-line-100554"
+    ],
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html",
+            "type": "function"
+          },
+          {
+            "name": "css",
+            "type": "function"
+          },
+          {
+            "name": "LitElement",
+            "type": "class"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement",
+            "type": "function"
+          },
+          {
+            "name": "property",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/stateLitElement.js",
+        "dependencies": [
+          {
+            "name": "StateLitElement",
+            "type": "class"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "A simple divider line widget",
+      "businessCapabilities": [],
+      "technicalCapabilities": [
+        "Renders an HTML hr element"
+      ],
+      "implementedFeatures": [
+        "Displays a horizontal rule"
+      ]
+    }
+  }
+}
+    
