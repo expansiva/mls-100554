e48cf864028cc58ed161eaef0a25ff31b2170d71
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:43:23 -0300

    save: l2/widgetDivider.defs.ts

--- l2/widgetDivider.defs.ts
+++ l2/widgetDivider.defs.ts
@@ -1,2 +1,68 @@
-/// <mls shortName="widgetDivider" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/widgetDivider.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/widgetDivider.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html",
+            "type": "function"
+          },
+          {
+            "name": "css",
+            "type": "function"
+          },
+          {
+            "name": "LitElement",
+            "type": "class"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement",
+            "type": "function"
+          },
+          {
+            "name": "property",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/stateLitElement.js",
+        "dependencies": [
+          {
+            "name": "StateLitElement",
+            "type": "class"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Widget Divider Component",
+      "businessCapabilities": [],
+      "technicalCapabilities": [
+        "Renders an HR element with optional text"
+      ],
+      "implementedFeatures": [
+        "Displays a divider with customizable text"
+      ]
+    }
+  }
+}
+    
