58fe067dffff96f2a6b9ff2c8069c165962583e7
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 16:59:58 -0300

    save: l2/collabStartL3.defs.ts

--- l2/collabStartL3.defs.ts
+++ l2/collabStartL3.defs.ts
@@ -1,2 +1,74 @@
-/// <mls shortName="collabStartL3" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/collabStartL3.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/collabStartL3.ts",
+    "componentType": "page",
+    "componentScope": "appFrontEnd",
+    "languages": [
+      "en",
+      "pt"
+    ],
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/stateLitElement.js",
+        "dependencies": [
+          {
+            "name": "StateLitElement",
+            "type": "class"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "L3 Module Start Page",
+      "businessCapabilities": [
+        "Selecionar Componentes de Interface (Widgets): permite escolher widgets e componentes de interface que serão usados nas páginas do projeto.",
+        "Selecionar ou Criar Interfaces de Componentes e Páginas (CSS):oferece a opção de utilizar estilos CSS pré-definidos ou criar novos, de acordo com as necessidades específicas do projeto.",
+        "Selecionar Ícones e Imagens: permite escolher ícones e imagens que se alinham com a estética e os objetivos do projeto.",
+        "Editar Documentação do Projeto: espaço para documentar regras de design, boas práticas e outros guidelines que devem ser seguidos pela equipe.",
+        "Ajustar Layout e visual da Página: espaço para o Designer deixar a página de uma forma agradável para o usuário.",
+        "O usuário começa selecionando os componentes de interface que serão utilizados nas páginas.",
+        "Em seguida, ajusta ou cria novos estilos CSS para esses componentes e para as páginas em geral.",
+        "Seleciona ícones e imagens que complementam o design.",
+        "Finaliza documentando as escolhas de design, regras e boas práticas para garantir consistência ao longo do projeto.",
+        "O módulo L3 é fundamental para garantir que o projeto tenha uma aparência e sensação consistentes, alinhadas com a visão de negócios e as expectativas do usuário. Ele serve como um guia para todos os outros membros da equipe sobre como os elementos de design devem ser implementados e utilizados."
+      ],
+      "technicalCapabilities": [
+        "Lit web component",
+        "i18n support"
+      ],
+      "implementedFeatures": [
+        "Renders banner image",
+        "Displays details about L3 module",
+        "Supports multiple languages"
+      ]
+    }
+  }
+}
+    
