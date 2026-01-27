0eba1494b16aab79c5d507cc8630b8492c989fb1
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:00:01 -0300

    save: l2/collabStartL5.defs.ts

--- l2/collabStartL5.defs.ts
+++ l2/collabStartL5.defs.ts
@@ -1,2 +1,67 @@
-/// <mls shortName="collabStartL5" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/collabStartL5.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/collabStartL5.ts",
+    "componentType": "page",
+    "componentScope": "appFrontEnd",
+    "languages": [
+      "en",
+      "pt"
+    ],
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/stateLitElement.js",
+        "dependencies": [
+          {
+            "name": "StateLitElement",
+            "type": "class"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Module L5 - Management: Description and Features",
+      "businessCapabilities": [
+        "Visualização de Páginas do Projeto",
+        "Infraestrutura",
+        "Visualização de Recursos",
+        "Fluxo de Uso"
+      ],
+      "technicalCapabilities": [
+        "Lit web component",
+        "i18n support for English and Portuguese"
+      ],
+      "implementedFeatures": [
+        "Rendering of module description with details and news sections",
+        "Internationalization using message objects"
+      ]
+    }
+  }
+}
+    
