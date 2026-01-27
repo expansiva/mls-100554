4c116d74f0ccb7baf4ce9880bff6c5285a49d685
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:00:45 -0300

    save: l2/libProjectConfig.defs.ts

--- l2/libProjectConfig.defs.ts
+++ l2/libProjectConfig.defs.ts
@@ -1,2 +1,39 @@
-/// <mls shortName="libProjectConfig" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/libProjectConfig.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/libProjectConfig.ts",
+    "componentType": "tool",
+    "componentScope": "editor",
+    "group": "enhancement",
+    "devFidelity": "final"
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Library for project configuration",
+      "businessCapabilities": [
+        "clearLocalChanges",
+        "getConfigProject",
+        "updateConfigProject",
+        "updateConfigProjectPlugins",
+        "createConfigFile"
+      ],
+      "technicalCapabilities": [
+        "async functions",
+        "JSON parsing",
+        "file storage operations"
+      ],
+      "implementedFeatures": [
+        "clearLocalChanges",
+        "getConfigProject",
+        "updateConfigProject",
+        "updateConfigProjectPlugins",
+        "createConfigFile",
+        "_createConfigFile"
+      ]
+    }
+  }
+}
+    
