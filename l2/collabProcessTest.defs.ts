0c92d14227e0d63c1ca792f6b59d29e2865cc203
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 16:13:40 -0300

    save: l2/collabProcessTest.defs.ts

--- l2/collabProcessTest.defs.ts
+++ l2/collabProcessTest.defs.ts
@@ -1,2 +1,80 @@
-/// <mls shortName="collabProcessTest" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/collabProcessTest.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/collabProcessTest.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement",
+            "type": "function"
+          },
+          {
+            "name": "property",
+            "type": "function"
+          },
+          {
+            "name": "query",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/\\_100554_/l2/collabLitElement.js",
+        "dependencies": [
+          {
+            "name": "CollabLitElement",
+            "type": "class"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/tsTestAST.js",
+        "dependencies": [
+          {
+            "name": "TsTestAst",
+            "type": "class"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Process Test",
+      "businessCapabilities": [
+        "Change function name in script",
+        "Save test with integration"
+      ],
+      "technicalCapabilities": [
+        "Renders form with inputs and textarea",
+        "Uses Lit for templating"
+      ],
+      "implementedFeatures": [
+        "changeNameFunc",
+        "onSave",
+        "createObjeTest",
+        "addInEditor"
+      ]
+    }
+  }
+}
+    
