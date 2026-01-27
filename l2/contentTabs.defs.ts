b85722d1407cd4946b87fa251accbfa24e29202f
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:00:16 -0300

    save: l2/contentTabs.defs.ts

--- l2/contentTabs.defs.ts
+++ l2/contentTabs.defs.ts
@@ -1,2 +1,71 @@
-/// <mls shortName="contentTabs" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/contentTabs.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/contentTabs.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html",
+            "type": "function"
+          },
+          {
+            "name": "unsafeHTML",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement",
+            "type": "function"
+          },
+          {
+            "name": "property",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/collabLitElement.js",
+        "dependencies": [
+          {
+            "name": "CollabLitElement",
+            "type": "class"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "ContentTabs component",
+      "businessCapabilities": [
+        "Display tabbed content",
+        "Handle tab selection"
+      ],
+      "technicalCapabilities": [
+        "Render navigation",
+        "Render content",
+        "Manage selected index"
+      ],
+      "implementedFeatures": [
+        "renderNav",
+        "renderContent",
+        "handleSelect"
+      ]
+    }
+  }
+}
+    
