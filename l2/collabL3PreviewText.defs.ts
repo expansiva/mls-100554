d84a2ea3d487311cb5f4dd99e33a352fb77f3aa5
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 16:13:21 -0300

    save: l2/collabL3PreviewText.defs.ts

--- l2/collabL3PreviewText.defs.ts
+++ l2/collabL3PreviewText.defs.ts
@@ -1,2 +1,67 @@
-/// <mls shortName="collabL3PreviewText" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/collabL3PreviewText.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/collabL3PreviewText.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement"
+          },
+          {
+            "name": "property"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/stateLitElement.js",
+        "dependencies": [
+          {
+            "name": "StateLitElement"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/collabDecorators.js",
+        "dependencies": [
+          {
+            "name": "propertyDataSource"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Custom element for previewing and editing text",
+      "businessCapabilities": [
+        "Handles text editing on click and blur"
+      ],
+      "technicalCapabilities": [
+        "Uses Lit for rendering editable content"
+      ],
+      "implementedFeatures": [
+        "Renders value as editable HTML",
+        "Fires events on selection and change"
+      ]
+    }
+  }
+}
+    
