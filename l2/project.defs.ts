298d0dd3807829c351940b9c369d92637f48687c
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:24:39 -0300

    save: l2/project.defs.ts

--- l2/project.defs.ts
+++ l2/project.defs.ts
@@ -1,2 +1,21 @@
-/// <mls shortName="project" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/project.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/project.ts",
+    "componentType": "other",
+    "componentScope": "editor",
+    "group": "enhancement"
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Exports projectConfig with empty modules array",
+      "businessCapabilities": [],
+      "technicalCapabilities": [],
+      "implementedFeatures": []
+    }
+  }
+}
+    
