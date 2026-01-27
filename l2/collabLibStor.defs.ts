0644c77a98dec9de0f84f060cd068f47cd8011a0
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 16:13:28 -0300

    save: l2/collabLibStor.defs.ts

--- l2/collabLibStor.defs.ts
+++ l2/collabLibStor.defs.ts
@@ -1,4 +1,83 @@
-/// <mls shortName="[shortName]" project="[project]" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabLibStor.ts" enhancement="_blank" />
 
-// TODO: InDevelpoment
-                
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/collabLibStor.ts",
+    "componentType": "tool",
+    "componentScope": "editor",
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "/_102027_/l2/utils",
+        "dependencies": [
+          {
+            "name": "convertFileNameToTag",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/collabLibModel.js",
+        "dependencies": [
+          {
+            "name": "createModel",
+            "type": "function"
+          },
+          {
+            "name": "createAllModels",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/libCommom.js",
+        "dependencies": [
+          {
+            "name": "getBaseTemplate",
+            "type": "function"
+          },
+          {
+            "name": "verifyNeedAddTripleslach",
+            "type": "function"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Library for storage file operations",
+      "businessCapabilities": [
+        "Create storage files",
+        "Delete files",
+        "Rename files",
+        "Clone files",
+        "Undo file changes"
+      ],
+      "technicalCapabilities": [
+        "File storage management",
+        "Model creation and disposal",
+        "Content replacement"
+      ],
+      "implementedFeatures": [
+        "createStorFile",
+        "createAllFiles",
+        "deleteFile",
+        "deleteAllFiles",
+        "renameFile",
+        "renameAllFiles",
+        "cloneFile",
+        "cloneAllFiles",
+        "undoFile",
+        "undoAllFiles",
+        "isNewNameValid",
+        "replaceTripleslashAndTag"
+      ]
+    }
+  }
+}
+    
