c13a92c9bc2c8e5482606d07c8644306c55ffbff
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2026-01-27 14:21:19 -0300

    save: l2/pluginExploreListAddL4.defs.ts

--- l2/pluginExploreListAddL4.defs.ts
+++ l2/pluginExploreListAddL4.defs.ts
@@ -1,2 +1,149 @@
-/// <mls shortName="pluginExploreListAddL4" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/pluginExploreListAddL4.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/pluginExploreListAddL4.ts",
+    "componentType": "pluginUI",
+    "componentScope": "appFrontEnd",
+    "languages": [
+      "en",
+      "pt"
+    ],
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html"
+          },
+          {
+            "name": "repeat"
+          },
+          {
+            "name": "TemplateResult"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement"
+          },
+          {
+            "name": "state"
+          },
+          {
+            "name": "property"
+          },
+          {
+            "name": "query"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/collabLibStor.js",
+        "dependencies": [
+          {
+            "name": "createAllFiles"
+          },
+          {
+            "name": "IReqCreateAllFiles"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/pluginNewFileBase.js",
+        "dependencies": [
+          {
+            "name": "getTemplateImport"
+          }
+        ]
+      },
+      {
+        "ref": "/_102027_/l2/utils.js",
+        "dependencies": [
+          {
+            "name": "convertFileNameToTag"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/libCommom.js",
+        "dependencies": [
+          {
+            "name": "getInstanceByFile"
+          },
+          {
+            "name": "isNameValid"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/aiAgentHelper.js",
+        "dependencies": [
+          {
+            "name": "executeAgentByFile"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/collabImport.js",
+        "dependencies": [
+          {
+            "name": "collabImport"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/pluginBaseModule.js",
+        "dependencies": [
+          {
+            "name": "PluginBaseModule"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/serviceBase.js",
+        "dependencies": [
+          {
+            "name": "ServiceBase"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Plugin component for adding new pages or organisms",
+      "businessCapabilities": [
+        "Prepares module list",
+        "Creates new page files",
+        "Executes AI agent for prototype generation"
+      ],
+      "technicalCapabilities": [
+        "Uses Lit for rendering",
+        "Handles form inputs",
+        "Integrates with storage and AI helpers"
+      ],
+      "implementedFeatures": [
+        "prepare",
+        "init",
+        "render",
+        "createFile",
+        "goBack",
+        "verifyModuleConfig",
+        "setHistory",
+        "getNewNameAndValid",
+        "showLoad",
+        "showError"
+      ]
+    }
+  }
+}
+    
