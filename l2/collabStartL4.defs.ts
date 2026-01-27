a35326b651010e6a604ad0dbfe4c8c5c5dee04e4
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 16:59:59 -0300

    save: l2/collabStartL4.defs.ts

--- l2/collabStartL4.defs.ts
+++ l2/collabStartL4.defs.ts
@@ -1,2 +1,65 @@
-/// <mls shortName="collabStartL4" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/collabStartL4.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/collabStartL4.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "languages": [
+      "en",
+      "pt"
+    ],
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement"
+          }
+        ]
+      },
+      {
+        "ref": "/\\_100554_/l2/stateLitElement.js",
+        "dependencies": [
+          {
+            "name": "StateLitElement"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Module L4 - Business: Description and Features",
+      "businessCapabilities": [
+        "Design Page in Wireframe Mode",
+        "Use Design System (UI)",
+        "Use Components",
+        "Use Back-End Features",
+        "Program Page Features",
+        "Test and Release for Approval/Publish"
+      ],
+      "technicalCapabilities": [
+        "Lit web component",
+        "i18n support"
+      ],
+      "implementedFeatures": [
+        "Render section with banner, content details, and collab marca image"
+      ]
+    }
+  }
+}
+    
