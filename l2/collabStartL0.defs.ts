d275c6cc998c585ea92efcee608302a661242d07
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 16:59:55 -0300

    save: l2/collabStartL0.defs.ts

--- l2/collabStartL0.defs.ts
+++ l2/collabStartL0.defs.ts
@@ -1,2 +1,74 @@
-/// <mls shortName="collabStartL0" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/collabStartL0.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/collabStartL0.ts",
+    "componentType": "page",
+    "componentScope": "appFrontEnd",
+    "languages": [
+      "en",
+      "pt"
+    ],
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/stateLitElement.js",
+        "dependencies": [
+          {
+            "name": "StateLitElement",
+            "type": "class"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Module L0 - User: Description and Features",
+      "businessCapabilities": [
+        "Visualizar e ver projetos: permite ao usuário uma visão geral e detalhada dos projetos em que está envolvido, facilitando o acompanhamento do progresso",
+        "Dar autoridades a outros: oferece a opção de conceder diferentes níveis de autoridade a outros membros da equipe, como administrador, editor ou visualizador",
+        "Escolher o plano atual: permite ao usuário selecionar o plano de assinatura mais adequado às suas necessidades e, se necessário, incluir informações de faturamento.",
+        "Selecionar preferências visuais: oferece opções para personalizar a aparência da ferramenta, como temas, layout e configurações de exibição.",
+        "View and monitor projects: allows the user to have an overview and detailed insight into the projects they are involved in, facilitating progress tracking.",
+        "Grant authorities to others: offers the option to assign different levels of authority to other team members, such as administrator, editor, or viewer.",
+        "Select the current plan: enables the user to choose the subscription plan that best suits their needs and, if necessary, include billing information.",
+        "Choose visual preferences: provides options to customize the tool’s appearance, such as themes, layout, and display settings."
+      ],
+      "technicalCapabilities": [],
+      "implementedFeatures": [
+        "O usuário acessa o módulo L0 e tem uma visão geral dos projetos em que está envolvido.",
+        "Se necessário, concede autoridades a outros membros da equipe para facilitar a colaboração.",
+        "Escolhe o plano de assinatura que melhor atende às suas necessidades e inclui informações de faturamento, se aplicável..",
+        "Personaliza a interface da ferramenta de acordo com suas preferências visuais para uma experiência de usuário mais agradável.",
+        "The user accesses the L0 module and gets an overview of the projects they are involved in.",
+        "If needed, grants authority to other team members to facilitate collaboration.",
+        "Selects the subscription plan that best meets their needs and includes billing information, if applicable.",
+        "Customizes the tool’s interface according to their visual preferences for a more pleasant user experience."
+      ]
+    }
+  }
+}
+    
