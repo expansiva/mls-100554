cad484b7a27cc6f15ec4838409226752707eed64
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-01-27 17:43:28 -0300

    save: l2/widgetInputText.defs.ts

--- l2/widgetInputText.defs.ts
+++ l2/widgetInputText.defs.ts
@@ -1,2 +1,73 @@
-/// <mls shortName="widgetInputText" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/widgetInputText.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_100554_/l2/widgetInputText.ts",
+    "componentType": "molecule",
+    "componentScope": "appFrontEnd",
+    "group": "enhancement"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "html",
+            "type": "function"
+          },
+          {
+            "name": "ifDefined",
+            "type": "function"
+          },
+          {
+            "name": "LitElement",
+            "type": "class"
+          }
+        ]
+      },
+      {
+        "ref": "lit/decorators.js",
+        "dependencies": [
+          {
+            "name": "customElement",
+            "type": "function"
+          },
+          {
+            "name": "property",
+            "type": "function"
+          },
+          {
+            "name": "query",
+            "type": "function"
+          }
+        ]
+      },
+      {
+        "ref": "/_100554_/l2/collabDecorators.js",
+        "dependencies": [
+          {
+            "name": "propertyDataSource",
+            "type": "function"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Text input widget component",
+      "businessCapabilities": [],
+      "technicalCapabilities": [
+        "Renders a text input field with properties and event handling"
+      ],
+      "implementedFeatures": [
+        "Text input with label, placeholder, validation, and change handling"
+      ],
+      "constraints": []
+    }
+  }
+}
+    
