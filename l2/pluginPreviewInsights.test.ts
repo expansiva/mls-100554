78b90ef48f2a3cd1280756ea1924de3f8f704a59
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2026-01-28 11:01:18 -0300

    delete: l2/pluginPreviewInsights.test.ts

--- l2/pluginPreviewInsights.test.ts
+++
@@ -1,6 +0,0 @@
-/// <mls shortName="pluginPreviewInsights" project="100554" enhancement="_blank" />
-				
-import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js'; 
-
-export const integrations: ICANIntegration[] = []; 
-export const tests: ICANTest[] = [];
