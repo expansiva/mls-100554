898003ed632448c3fee9ec3e3ce107bb63bb7958
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2026-01-28 09:48:36 -0300

    save: l2/serviceSource.ts

--- l2/serviceSource.ts
+++ l2/serviceSource.ts
@@ -157,7 +157,7 @@ export class ServiceSource100554 extends ServiceBase {
 
             if (!stor) {
 
-                let template = getBaseTemplate({ folder, shortName, project, extension: ext });
+                let template = await getBaseTemplate({ folder, shortName, project, extension: ext });
 
                 const param: IReqCreateStorFile = {
                     project,
@@ -204,7 +204,7 @@ export class ServiceSource100554 extends ServiceBase {
 
             if (!stor) {
 
-                let template = getBaseTemplate({ folder, shortName, project, extension: ext });
+                let template = await getBaseTemplate({ folder, shortName, project, extension: ext });
 
                 const param: IReqCreateStorFile = {
                     project,
