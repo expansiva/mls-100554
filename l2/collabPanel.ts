b988c63ee62e7538556831bf39b732c6cb6bef0d
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:31:40 -0300

    save: l2/collabPanel.ts

--- l2/collabPanel.ts
+++ l2/collabPanel.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabPanel" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/collabPanel.ts" enhancement="_blank" />
 
 import { html, css, UnsafeHTMLDirective, repeat, unsafeHTML } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
