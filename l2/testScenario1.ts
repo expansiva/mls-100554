1eeeebf5bf5173e4ec28f34da5edbcc1866f349c
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:37:13 -0300

    save: l2/testScenario1.ts

--- l2/testScenario1.ts
+++ l2/testScenario1.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="testScenario1" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/testScenario1.ts" groupName="other" enhancement="_100554_enhancementLit" />
 
 import { IScenaryDetails } from '/_100554_/l2/collabLitElement.js'; 
 export function _100554_testScenario1_getScenaryDetails(): IScenaryDetails {
