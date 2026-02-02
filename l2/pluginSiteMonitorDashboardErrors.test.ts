79146421e87d4a3391524dfc56de75387c7f3498
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:11:58 -0300

    save: l2/pluginSiteMonitorDashboardErrors.test.ts

--- l2/pluginSiteMonitorDashboardErrors.test.ts
+++ l2/pluginSiteMonitorDashboardErrors.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="pluginSiteMonitorDashboardErrors" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/pluginSiteMonitorDashboardErrors.test.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
