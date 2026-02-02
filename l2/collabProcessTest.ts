d17a5c323722f6e7add35a6ac20e9523e85b0d95
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:45:38 -0300

    save: l2/collabProcessTest.ts

--- l2/collabProcessTest.ts
+++ l2/collabProcessTest.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabProcessTest.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabProcessTest.ts" enhancement="_100554_enhancementLit" />
 
 import { html } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
