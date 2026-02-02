f4e7aa99c0cd0dc208670ef63a02bc54714b066d
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:26:45 -0300

    save: l2/pluginSiteMonitorDashboardSpikes.ts

--- l2/pluginSiteMonitorDashboardSpikes.ts
+++ l2/pluginSiteMonitorDashboardSpikes.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginSiteMonitorDashboardSpikes.ts" group="other" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/pluginSiteMonitorDashboardSpikes.ts" groupName="other" enhancement="_100554_enhancementLit" />
 
 import { html, css, svg, TemplateResult } from 'lit';
 import { query, property } from 'lit/decorators.js';
