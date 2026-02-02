6c9ccecf45439010cbfbec608e9f2b0b0a28e70c
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:46:00 -0300

    save: l2/collabTokensVisualization.ts

--- l2/collabTokensVisualization.ts
+++ l2/collabTokensVisualization.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabTokensVisualization.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabTokensVisualization.ts" enhancement="_100554_enhancementLit" />
 
 import { html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
