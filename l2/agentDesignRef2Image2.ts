3ef82188621de81d153858fd0e0512e9d4d644c8
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:12:56 -0300

    save: l2/agentDesignRef2Image2.ts

--- l2/agentDesignRef2Image2.ts
+++ l2/agentDesignRef2Image2.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="agentDesignRef2Image2" project="100554" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/agentDesignRef2Image2.ts" enhancement="_blank" />
 
 import { IAgent, svg_agent } from '/_100554_/l2/aiAgentBase.js';
 import { getPromptByHtml } from '/_100554_/l2/aiPrompts.js';
