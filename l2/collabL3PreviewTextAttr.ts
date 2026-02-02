cbb1f8016f5915700db10845e0bbc1e26ad15040
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:30:58 -0300

    save: l2/collabL3PreviewTextAttr.ts

--- l2/collabL3PreviewTextAttr.ts
+++ l2/collabL3PreviewTextAttr.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabL3PreviewTextAttr" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/collabL3PreviewTextAttr.ts" enhancement="_blank" />
 
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
