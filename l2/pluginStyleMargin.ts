bb824f19f2f4ac56d6cdcb854e0a6557bdff2793
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:13:14 -0300

    save: l2/pluginStyleMargin.ts

--- l2/pluginStyleMargin.ts
+++ l2/pluginStyleMargin.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="pluginStyleMargin" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/pluginStyleMargin.ts" group="other" enhancement="_100554_enhancementLit" />
 
 import { html, repeat } from 'lit';
 import { customElement, property, query, queryAll } from 'lit/decorators.js';
