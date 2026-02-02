5f3ad472144e8ff7ffa1323fc55518bc11f868dc
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:35:53 -0300

    save: l2/serviceMindMap.ts

--- l2/serviceMindMap.ts
+++ l2/serviceMindMap.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="serviceMindMap" project="100554" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/serviceMindMap.ts" enhancement="_100554_enhancementLit" />
 
 import { html, unsafeHTML, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
