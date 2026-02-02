26e89dfbaf478cc3602c12624c054dfe406bd9e4
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:38:12 -0300

    save: l2/widgetDivider.ts

--- l2/widgetDivider.ts
+++ l2/widgetDivider.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="widgetDivider" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/widgetDivider.ts" groupName="other" enhancement="_100554_enhancementLit" />
 
 import { html, css, LitElement } from 'lit'; 
 import { customElement, property } from 'lit/decorators.js';
