9ca9b32669d2ef5ac489a6e31cd1dbc619b3d4c1
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:26:55 -0300

    save: l2/pluginStyleColumn.ts

--- l2/pluginStyleColumn.ts
+++ l2/pluginStyleColumn.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginStyleColumn.ts" group="other" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/pluginStyleColumn.ts" groupName="other" enhancement="_100554_enhancementLit" />
 
 import { html, repeat } from 'lit';
 import { customElement, property, queryAll } from 'lit/decorators.js';
