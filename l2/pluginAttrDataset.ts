de61cf83c0579cd2b42b4e0d7a50c4f21ca2a793
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:25:26 -0300

    save: l2/pluginAttrDataset.ts

--- l2/pluginAttrDataset.ts
+++ l2/pluginAttrDataset.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginAttrDataset.ts" group="other" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/pluginAttrDataset.ts" groupName="other" enhancement="_100554_enhancementLit" />
 
 import { html, css, svg, repeat, TemplateResult } from 'lit';
 import { property, state } from 'lit/decorators.js';
