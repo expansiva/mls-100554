5591fe4c28a34ffe057478e0f66a748ee9dd94a1
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:14:15 -0300

    save: l2/aiAgentBase.ts

--- l2/aiAgentBase.ts
+++ l2/aiAgentBase.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="aiAgentBase" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/aiAgentBase.ts" enhancement="_blank" />
 
 import { TemplateResult } from 'lit'; 
 
