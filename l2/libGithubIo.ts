1e9fd9c5c1b1e3457cc9a6903ce225ffa1839587
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:56:37 -0300

    save: l2/libGithubIo.ts

--- l2/libGithubIo.ts
+++ l2/libGithubIo.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="libGithubIo" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/libGithubIo.ts" group="other" enhancement="_100554_enhancementLit" />
 
 export async function addIssueInProject(req: IReq, idProject: string, idIssue: string): Promise<string | undefined> {
 
