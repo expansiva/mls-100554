3c128390e5cc9a0b9a9ac17925091f7a16b79979
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:45:10 -0300

    save: l2/collabDsInputSelectColor.ts

--- l2/collabDsInputSelectColor.ts
+++ l2/collabDsInputSelectColor.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabDsInputSelectColor.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabDsInputSelectColor.ts" enhancement="_100554_enhancementLit" />
 
 import { html, css, repeat } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
