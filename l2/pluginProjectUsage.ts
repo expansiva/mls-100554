b36b9ed9fcae38acb8134844e51667490a41053c
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:26:33 -0300

    save: l2/pluginProjectUsage.ts

--- l2/pluginProjectUsage.ts
+++ l2/pluginProjectUsage.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginProjectUsage.ts" group="other" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/pluginProjectUsage.ts" groupName="other" enhancement="_100554_enhancementLit" />
 
 import { html, css, svg, TemplateResult } from 'lit';
 import { query, property } from 'lit/decorators.js';
