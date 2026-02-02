aa06dffc243ea5b878d1a6c7da13c2570650e354
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:36:22 -0300

    save: l2/servicePreviewView.ts

--- l2/servicePreviewView.ts
+++ l2/servicePreviewView.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="servicePreviewView" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/servicePreviewView.ts" groupName="other" enhancement="_100554_enhancementLit" />
 
 import { html, unsafeHTML } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
