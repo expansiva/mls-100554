151e984aa06c27676bfba1d8a30e4a8bb0bcb6fe
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:35:35 -0300

    save: l2/serviceDetail.ts

--- l2/serviceDetail.ts
+++ l2/serviceDetail.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="serviceDetail" project="100554" enhancement="_100554_enhancementLitService" groupName="other" />
+/// <mls fileReference="_100554_/l2/serviceDetail.ts" groupName="other" enhancement="_100554_enhancementLitService" />
 
 import { html, css } from 'lit';
 import { customElement, property, query, state } from 'lit/decorators.js';
