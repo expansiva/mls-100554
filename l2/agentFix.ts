7b3ddc6fa415e3e2e6f9ae13540397c9eafa9d41
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:13:00 -0300

    save: l2/agentFix.ts

--- l2/agentFix.ts
+++ l2/agentFix.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="agentFix" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/agentFix.ts" enhancement="_blank" />
 
 import { IAgent } from '/_100554_/l2/aiAgentBase.js';
 import {
