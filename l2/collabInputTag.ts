256afb53c184b397649a843d46f7b20f04fab800
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:45:17 -0300

    save: l2/collabInputTag.ts

--- l2/collabInputTag.ts
+++ l2/collabInputTag.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabInputTag.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabInputTag.ts" enhancement="_100554_enhancementLit" />
 
 import { html, ifDefined } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
