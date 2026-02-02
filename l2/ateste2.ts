a017d26fc42831e126e42c1d536c307fc77b7ca3
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:55:03 -0300

    save: l2/ateste2.ts

--- l2/ateste2.ts
+++ l2/ateste2.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="ateste2" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/ateste2.ts" group="other" enhancement="_100554_enhancementLit" />
 
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
