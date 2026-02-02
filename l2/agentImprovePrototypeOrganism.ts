2c4669b029cfa6b24d9fa5b740cc7443206c1d8a
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:13:30 -0300

    save: l2/agentImprovePrototypeOrganism.ts

--- l2/agentImprovePrototypeOrganism.ts
+++ l2/agentImprovePrototypeOrganism.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="agentImprovePrototypeOrganism" project="100554" enhancement="_100554_enhancementLit" groupName="other" folder="" />
+/// <mls fileReference="_100554_/l2/agentImprovePrototypeOrganism.ts" enhancement="_blank" />
 
 import { IAgent, svg_agent } from '/_100554_/l2/aiAgentBase.js';
 import { getPromptByHtml } from '/_100554_/l2/aiPrompts.js';
