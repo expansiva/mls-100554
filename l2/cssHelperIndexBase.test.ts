9b8df3e968ee4fd269fdd260f261dc6491350458
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:34:01 -0300

    save: l2/cssHelperIndexBase.test.ts

--- l2/cssHelperIndexBase.test.ts
+++ l2/cssHelperIndexBase.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="cssHelperIndexBase" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/cssHelperIndexBase.test.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
