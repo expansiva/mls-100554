3ca78bfdbed923687b481aaf5bc67135e0fa1711
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:45:53 -0300

    save: l2/collabStartL5.ts

--- l2/collabStartL5.ts
+++ l2/collabStartL5.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabStartL5.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabStartL5.ts" enhancement="_100554_enhancementLit" />
 
 import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
