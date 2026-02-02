9397ad27ddad7fa22f79811c8ab247e78223d242
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:32:24 -0300

    save: l2/collabSpliterHorizontalVarFixed.ts

--- l2/collabSpliterHorizontalVarFixed.ts
+++ l2/collabSpliterHorizontalVarFixed.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabSpliterHorizontalVarFixed" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/collabSpliterHorizontalVarFixed.ts" enhancement="_blank" />
 
 import { html, css, LitElement } from 'lit';
 import { customElement, property, queryAll } from 'lit/decorators.js';
