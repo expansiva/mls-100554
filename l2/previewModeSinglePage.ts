4bf08b50171ecaef1f0ee4f99f59d611770f77ae
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:27:34 -0300

    save: l2/previewModeSinglePage.ts

--- l2/previewModeSinglePage.ts
+++ l2/previewModeSinglePage.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/previewModeSinglePage.ts" group="other" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/previewModeSinglePage.ts" groupName="other" enhancement="_100554_enhancementLit" />
 
 import { IJSONDependence } from '/_100554_/l2/libCompile.js';
 import { setErrorOnModel, convertTagToFileName } from '/_102027_/l2/utils.js';
