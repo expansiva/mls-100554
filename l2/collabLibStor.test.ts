5ca978902ee74c4f2b07db7e410782f4dff1960e
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:31:16 -0300

    save: l2/collabLibStor.test.ts

--- l2/collabLibStor.test.ts
+++ l2/collabLibStor.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabLibStor" project="100554" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabLibStor.test.ts" enhancement="_blank" />
 				
 import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js'; 
 
