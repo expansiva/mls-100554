bf1616ba59862106def9fdf75c56e96dffbf4f5b
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:09:46 -0300

    save: l2/pluginNavigationRenderOrganism.ts

--- l2/pluginNavigationRenderOrganism.ts
+++ l2/pluginNavigationRenderOrganism.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="pluginNavigationRenderOrganism" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/pluginNavigationRenderOrganism.ts" group="other" enhancement="_100554_enhancementLit" />
 
 import { html, repeat, TemplateResult, LitElement } from 'lit';
 import { customElement, state, property, query } from 'lit/decorators.js';
