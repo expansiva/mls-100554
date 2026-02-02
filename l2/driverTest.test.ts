8975a4366e8eb70328d69a4f7249a7e096818998
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:55:36 -0300

    save: l2/driverTest.test.ts

--- l2/driverTest.test.ts
+++ l2/driverTest.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="driverTest" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/driverTest.test.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
