e3a34948c7d43ce464cc1ad59ebbbf3a83a5d45b
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:11:46 -0300

    save: l2/pluginPrototypeImprove.ts

--- l2/pluginPrototypeImprove.ts
+++ l2/pluginPrototypeImprove.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="pluginPrototypeImprove" project="100554" enhancement="_100554_enhancementLit" groupName="other" folder="" />
+/// <mls fileReference="_100554_/l2/pluginPrototypeImprove.ts" group="other" enhancement="_100554_enhancementLit" />
 
 import { html } from 'lit';
 import { customElement, state, property } from 'lit/decorators.js';
