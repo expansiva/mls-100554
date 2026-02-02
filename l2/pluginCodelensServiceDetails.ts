7a69cb20d672a41c32f63fcd571714afd3faaaa2
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:08:16 -0300

    save: l2/pluginCodelensServiceDetails.ts

--- l2/pluginCodelensServiceDetails.ts
+++ l2/pluginCodelensServiceDetails.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="pluginCodelensServiceDetails" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/pluginCodelensServiceDetails.ts" group="other" enhancement="_100554_enhancementLit" />
 
 import { html, unsafeHTML, LitElement } from 'lit';
 import { customElement, } from 'lit/decorators.js';
