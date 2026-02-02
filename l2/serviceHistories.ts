badf8007a93afeef23b03176a9cb52a13c299de0
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:35:44 -0300

    save: l2/serviceHistories.ts

--- l2/serviceHistories.ts
+++ l2/serviceHistories.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="serviceHistories" project="100554" enhancement="_100554_enhancementLit" groupName="service" />
+/// <mls fileReference="_100554_/l2/serviceHistories.ts" groupName="service" enhancement="_100554_enhancementLit" />
 
 import { html } from 'lit';
 import { customElement, query, property, state } from 'lit/decorators.js';
