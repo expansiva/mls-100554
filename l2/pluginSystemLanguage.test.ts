0054db3530790cc0cf622b2690994d1551a15f3a
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:13:36 -0300

    save: l2/pluginSystemLanguage.test.ts

--- l2/pluginSystemLanguage.test.ts
+++ l2/pluginSystemLanguage.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="pluginSystemLanguage" project="100554" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginSystemLanguage.test.ts" enhancement="_blank" />
 				
 import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js'; 
 
