15cef0a80bab4327d3ab95c2f75d4461fb32d357
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:10:14 -0300

    save: l2/pluginNewProjectLog.ts

--- l2/pluginNewProjectLog.ts
+++ l2/pluginNewProjectLog.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="pluginNewProjectLog" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/pluginNewProjectLog.ts" group="other" enhancement="_100554_enhancementLit" />
 
 import { html, css, LitElement, TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
