84acf470b0bd6c129570646bfb6914336f0b8e70
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:12:46 -0300

    save: l2/agentBotWeddingGifts.ts

--- l2/agentBotWeddingGifts.ts
+++ l2/agentBotWeddingGifts.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="agentBotWeddingGifts" project="100554" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/agentBotWeddingGifts.ts" enhancement="_blank" />
 
 
 import { IAgent, svg_agent } from '/_100554_/l2/aiAgentBase.js';
