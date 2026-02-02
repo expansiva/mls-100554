daa4c7cbbd2aecea0f674ab36094a1f9cdcb603b
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:32:42 -0300

    save: l2/collabStartL2.test.ts

--- l2/collabStartL2.test.ts
+++ l2/collabStartL2.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabStartL2" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/collabStartL2.test.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
