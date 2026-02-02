8e19c6f3d82fd1012578142b4baac3e6ab8edea5
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:13:08 -0300

    save: l2/pluginStyleIndexItem.test.ts

--- l2/pluginStyleIndexItem.test.ts
+++ l2/pluginStyleIndexItem.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="pluginStyleIndexItem" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/pluginStyleIndexItem.test.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
