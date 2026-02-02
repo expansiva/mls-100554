fe79f6017a14cef24060570fd760e52e40e269cd
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:33:22 -0300

    save: l2/collabTasks.ts

--- l2/collabTasks.ts
+++ l2/collabTasks.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabTasks" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/collabTasks.ts" enhancement="_blank" />
 
 import { html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
