36fd40b1be5f7dad5c74e43a741bf6711d6c3700
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:11:31 -0300

    save: l2/pluginProjectReadMe.ts

--- l2/pluginProjectReadMe.ts
+++ l2/pluginProjectReadMe.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="pluginProjectReadMe" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/pluginProjectReadMe.ts" group="other" enhancement="_100554_enhancementLit" />
 
 import { html, css, svg, TemplateResult } from 'lit';
 import { query, property } from 'lit/decorators.js';
