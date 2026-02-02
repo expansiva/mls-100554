5c03d9cc30a4e54d394d52357af0fd0affc6109a
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:38:33 -0300

    save: l2/widgetInputText.test.ts

--- l2/widgetInputText.test.ts
+++ l2/widgetInputText.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="widgetInputText" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/widgetInputText.test.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
