5c334aca0fa0b28cc3c8c5009610092284e04455
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:37:42 -0300

    save: l2/tsTestASTTeste.ts

--- l2/tsTestASTTeste.ts
+++ l2/tsTestASTTeste.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="tsTestASTTeste" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/tsTestASTTeste.ts" groupName="other" enhancement="_100554_enhancementLit" />
 
 import { html, css, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
