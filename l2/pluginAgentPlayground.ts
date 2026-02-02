a5451192260c66718c71ece2fd2e8483bc95203f
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:07:43 -0300

    save: l2/pluginAgentPlayground.ts

--- l2/pluginAgentPlayground.ts
+++ l2/pluginAgentPlayground.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="pluginAgentPlayground" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/pluginAgentPlayground.ts" group="other" enhancement="_100554_enhancementLit" />
 
 import { html, repeat, unsafeHTML } from 'lit';
 import { customElement, property, state, query } from 'lit/decorators.js';
