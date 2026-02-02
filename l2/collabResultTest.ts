65ed96a33cee1a0e9abf2f0980da38605e0cc9d8
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:32:09 -0300

    save: l2/collabResultTest.ts

--- l2/collabResultTest.ts
+++ l2/collabResultTest.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabResultTest" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/collabResultTest.ts" enhancement="_blank" />
 
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
