c43e68bfd7dce7e66bec5fc6420509656cf2ce6b
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 10:35:57 -0300

    save: l2/serviceOrganism.test.ts

--- l2/serviceOrganism.test.ts
+++ l2/serviceOrganism.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="serviceOrganism" project="100554" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/serviceOrganism.test.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
