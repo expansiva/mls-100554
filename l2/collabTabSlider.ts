0d0a3a984082cf3fecf8c53b9362df75769bdc80
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:33:17 -0300

    save: l2/collabTabSlider.ts

--- l2/collabTabSlider.ts
+++ l2/collabTabSlider.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabTabSlider" project="100554" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/collabTabSlider.ts" enhancement="_blank" />
 
 import { html, css, LitElement } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
