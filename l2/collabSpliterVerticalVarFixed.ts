83fc9f380b9999ed778be0ee6735d1793639905f
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:45:45 -0300

    save: l2/collabSpliterVerticalVarFixed.ts

--- l2/collabSpliterVerticalVarFixed.ts
+++ l2/collabSpliterVerticalVarFixed.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabSpliterVerticalVarFixed.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabSpliterVerticalVarFixed.ts" enhancement="_100554_enhancementLit" />
 
 import { html, css, LitElement } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
