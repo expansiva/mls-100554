a031b1da05d723b901e67ffa1a62122cca2384af
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:31:06 -0300

    save: l2/collabLanguages.test.ts

--- l2/collabLanguages.test.ts
+++ l2/collabLanguages.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabLanguages" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/collabLanguages.test.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
