6329eb98d9270d59981d01b07a66519b88cd450a
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:45:03 -0300

    save: l2/collabConsole.ts

--- l2/collabConsole.ts
+++ l2/collabConsole.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabConsole.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabConsole.ts" enhancement="_100554_enhancementLit" />
 
 import { html } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
