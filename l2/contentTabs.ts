3737738de6e07eba5d11ab1075329dd2758ba546
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:46:04 -0300

    save: l2/contentTabs.ts

--- l2/contentTabs.ts
+++ l2/contentTabs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/contentTabs.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/contentTabs.ts" enhancement="_100554_enhancementLit" />
 
 import { html, unsafeHTML } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
