d121744bdf627955da612d5d7f47b9006127bccc
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:29:46 -0300

    save: l2/coachMarks.ts

--- l2/coachMarks.ts
+++ l2/coachMarks.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="coachMarks" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/coachMarks.ts" enhancement="_blank" />
 
 import { html, css, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
