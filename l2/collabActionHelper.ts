601936c63cc89775c4bd11b34e63315b1e565bf4
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:45:02 -0300

    save: l2/collabActionHelper.ts

--- l2/collabActionHelper.ts
+++ l2/collabActionHelper.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabActionHelper.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabActionHelper.ts" enhancement="_100554_enhancementLit" />
 
 export const ambient: "RPC" | "" = "";
 
