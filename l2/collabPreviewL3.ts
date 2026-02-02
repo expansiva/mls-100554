47c997177f5cd645652aff4f01e9e03cee9fe1bc
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:31:55 -0300

    save: l2/collabPreviewL3.ts

--- l2/collabPreviewL3.ts
+++ l2/collabPreviewL3.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabPreviewL3" project="100554" enhancement="_100554_enhancementLit" groupName="other" folder="" />
+/// <mls fileReference="_100554_/l2/collabPreviewL3.ts" enhancement="_blank" />
 
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
