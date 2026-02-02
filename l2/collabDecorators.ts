3a16eb2e7b71d43ba58eb26c16b663b2798ff3df
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-02 09:30:10 -0300

    save: l2/collabDecorators.ts

--- l2/collabDecorators.ts
+++ l2/collabDecorators.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabDecorators" project="100554" enhancement="_100554_enhancementLit" groupName="other" />
+/// <mls fileReference="_100554_/l2/collabDecorators.ts" enhancement="_blank" />
 
 import { PropertyDeclaration } from 'lit';
 import { property } from 'lit/decorators.js';
