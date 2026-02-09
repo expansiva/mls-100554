d3aab58b5c18fc904ae154b65914617cf955c26f
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 09:57:40 -0300

    save: l2/pluginPresenterRecorder.ts

--- l2/pluginPresenterRecorder.ts
+++ l2/pluginPresenterRecorder.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginPresenterRecorder.ts" groupName="other" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/pluginPresenterRecorder.ts" enhancement="_100554_enhancementLit" />
 
 import { html, css, svg, TemplateResult, LitElement } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
