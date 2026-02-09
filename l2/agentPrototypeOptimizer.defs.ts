9ad997a7ba3dbb972d50870a7b9e6bd04c097e5f
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 11:06:56 -0300

    save: l2/agentPrototypeOptimizer.defs.ts

--- l2/agentPrototypeOptimizer.defs.ts
+++ l2/agentPrototypeOptimizer.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/agentPrototypeOptimizer.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/agentPrototypeOptimizer.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
