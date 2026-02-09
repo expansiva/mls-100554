5abd587352ade4cc84f432c62014e365819752b8
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:25:27 -0300

    save: l2/designSystem.defs.ts

--- l2/designSystem.defs.ts
+++ l2/designSystem.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/designSystem.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/designSystem.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
