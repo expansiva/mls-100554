3c358ff853713007ceeb7079509f4e661c5f8bea
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:31:42 -0300

    save: l2/pluginNewFileBlank.defs.ts

--- l2/pluginNewFileBlank.defs.ts
+++ l2/pluginNewFileBlank.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginNewFileBlank.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginNewFileBlank.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
