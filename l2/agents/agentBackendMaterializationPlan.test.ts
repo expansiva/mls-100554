40a5933165a2dc9d0dfbdbf1e635d13d85dc6fb1
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 09:21:30 -0300

    save: l2/agents/agentBackendMaterializationPlan.test.ts

--- l2/agents/agentBackendMaterializationPlan.test.ts
+++ l2/agents/agentBackendMaterializationPlan.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="agentBackendMaterializationPlan" project="100554" enhancement="_blank" folder="agents" />
+/// <mls fileReference="_100554_/l2/agents/agentBackendMaterializationPlan.defs.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
