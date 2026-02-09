21f5ea37ab926ff12df7378a617881e7db7839f7
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:24:19 -0300

    save: l2/collabDsInputSelectColor.defs.ts

--- l2/collabDsInputSelectColor.defs.ts
+++ l2/collabDsInputSelectColor.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabDsInputSelectColor.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabDsInputSelectColor.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
