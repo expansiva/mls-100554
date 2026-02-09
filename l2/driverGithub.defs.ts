a6e08ac148dd7966958a5883a4237f7d6e0db4db
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:25:31 -0300

    save: l2/driverGithub.defs.ts

--- l2/driverGithub.defs.ts
+++ l2/driverGithub.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/driverGithub.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/driverGithub.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
