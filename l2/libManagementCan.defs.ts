c04aa34562ce39d876d5680aa5a39772d95c0df0
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:30:57 -0300

    save: l2/libManagementCan.defs.ts

--- l2/libManagementCan.defs.ts
+++ l2/libManagementCan.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/libManagementCan.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/libManagementCan.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
