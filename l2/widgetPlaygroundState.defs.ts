ed4ca9f327139e398d6c9e1219d95bb91a4d544a
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:34:00 -0300

    save: l2/widgetPlaygroundState.defs.ts

--- l2/widgetPlaygroundState.defs.ts
+++ l2/widgetPlaygroundState.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/widgetPlaygroundState.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/widgetPlaygroundState.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
