080f0f9b3359cf5b3f281ed2872f77fe215d893f
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:31:10 -0300

    save: l2/pluginCodelensCustomElement.defs.ts

--- l2/pluginCodelensCustomElement.defs.ts
+++ l2/pluginCodelensCustomElement.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginCodelensCustomElement.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginCodelensCustomElement.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
