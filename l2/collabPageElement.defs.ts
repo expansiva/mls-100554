3eb6fa82ac4288250ff9cb7b8b10d6aecf8ee61f
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:24:41 -0300

    save: l2/collabPageElement.defs.ts

--- l2/collabPageElement.defs.ts
+++ l2/collabPageElement.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabPageElement.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabPageElement.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
