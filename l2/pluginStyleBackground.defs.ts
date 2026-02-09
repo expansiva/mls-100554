8bbfdecbd0eb6d37b105d72573660aa297c0a486
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:32:24 -0300

    save: l2/pluginStyleBackground.defs.ts

--- l2/pluginStyleBackground.defs.ts
+++ l2/pluginStyleBackground.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginStyleBackground.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginStyleBackground.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
