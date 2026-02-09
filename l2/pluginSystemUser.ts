4b8e84e5db54a5adb1f968cb05adac40640d600a
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 09:58:30 -0300

    save: l2/pluginSystemUser.ts

--- l2/pluginSystemUser.ts
+++ l2/pluginSystemUser.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginSystemUser.ts" groupName="other" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/pluginSystemUser.ts" enhancement="_100554_enhancementLit" />
 
 import { html, svg, TemplateResult } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
