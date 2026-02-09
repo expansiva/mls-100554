29b1ff15635951e7d34b82002249e16223e40f78
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:32:18 -0300

    save: l2/pluginSiteMonitorDashboardRegionalLatency.defs.ts

--- l2/pluginSiteMonitorDashboardRegionalLatency.defs.ts
+++ l2/pluginSiteMonitorDashboardRegionalLatency.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginSiteMonitorDashboardRegionalLatency.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginSiteMonitorDashboardRegionalLatency.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
