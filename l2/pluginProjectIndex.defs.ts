8bcba0fdbd17ce21a06276cbeb309eee1cd48b50
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:32:04 -0300

    save: l2/pluginProjectIndex.defs.ts

--- l2/pluginProjectIndex.defs.ts
+++ l2/pluginProjectIndex.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginProjectIndex.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginProjectIndex.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
