eece8f3dd13c44aaddb9a7f204eb9f6a3b4d5142
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 11:07:28 -0300

    save: l2/agents/agentUserJourney.ts

--- l2/agents/agentUserJourney.ts
+++ l2/agents/agentUserJourney.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/agents/agentUserJourney" enhancement="_100554_enhancementAgent"/>
+/// <mls fileReference="_100554_/l2/agents/agentUserJourney.ts" enhancement="_100554_enhancementAgent" />
 
 import { IAgentAsync, IAgentMeta } from '/_100554_/l2/aiAgentBase.js';
 
