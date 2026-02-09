ff7017887d52803b0ad9523cb75e5721b4a6150c
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:32:23 -0300

    save: l2/pluginSiteMonitorIndex.defs.ts

--- l2/pluginSiteMonitorIndex.defs.ts
+++ l2/pluginSiteMonitorIndex.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginSiteMonitorIndex.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginSiteMonitorIndex.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
