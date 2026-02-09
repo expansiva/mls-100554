3e13eb83c029a9ed68e080aed9eb0be45f83fb30
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:31:08 -0300

    save: l2/pluginBaseModule.defs.ts

--- l2/pluginBaseModule.defs.ts
+++ l2/pluginBaseModule.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginBaseModule.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginBaseModule.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
