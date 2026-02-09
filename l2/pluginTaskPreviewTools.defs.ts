6fd3b3b7a23bb27199411905be86178c18b24e40
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:32:57 -0300

    save: l2/pluginTaskPreviewTools.defs.ts

--- l2/pluginTaskPreviewTools.defs.ts
+++ l2/pluginTaskPreviewTools.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginTaskPreviewTools.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginTaskPreviewTools.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
