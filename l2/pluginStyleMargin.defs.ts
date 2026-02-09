7ccd58960b3d86cc3164101d2188c27fa132b8b0
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:32:36 -0300

    save: l2/pluginStyleMargin.defs.ts

--- l2/pluginStyleMargin.defs.ts
+++ l2/pluginStyleMargin.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginStyleMargin.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginStyleMargin.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
