6639ed44b99f7f74f6d95955b8ae55a46b32407f
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:31:39 -0300

    save: l2/pluginNavigationRenderOrganism.defs.ts

--- l2/pluginNavigationRenderOrganism.defs.ts
+++ l2/pluginNavigationRenderOrganism.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginNavigationRenderOrganism.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginNavigationRenderOrganism.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
