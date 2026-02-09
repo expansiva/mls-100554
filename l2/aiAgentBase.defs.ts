0e75d04c85949508629614cf2eca81ffcb57bfe2
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:23:56 -0300

    save: l2/aiAgentBase.defs.ts

--- l2/aiAgentBase.defs.ts
+++ l2/aiAgentBase.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/aiAgentBase.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/aiAgentBase.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
