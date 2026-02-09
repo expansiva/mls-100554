8abff73868b576f027cc7243c7d953e8db8b8858
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 10:04:48 -0300

    save: l2/testCoachMarks.ts

--- l2/testCoachMarks.ts
+++ l2/testCoachMarks.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/testCoachMarks.ts" groupName="other" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/testCoachMarks.ts" enhancement="_100554_enhancementLit" />
 
 import { CollabPageElement } from '/_100554_/l2/collabPageElement.js';
 import { customElement } from 'lit/decorators.js';
