0a9a5bbb4b6de5f96db6b653a812cad5dc3cafc4
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 09:58:21 -0300

    save: l2/pluginStyleTokens.ts

--- l2/pluginStyleTokens.ts
+++ l2/pluginStyleTokens.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginStyleTokens.ts" groupName="other" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/pluginStyleTokens.ts" enhancement="_100554_enhancementLit" />
 
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
