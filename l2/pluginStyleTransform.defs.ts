556b7e8a54cd0ff89ee6bbca1be1708a79b6000f
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:32:41 -0300

    save: l2/pluginStyleTransform.defs.ts

--- l2/pluginStyleTransform.defs.ts
+++ l2/pluginStyleTransform.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginStyleTransform.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginStyleTransform.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
