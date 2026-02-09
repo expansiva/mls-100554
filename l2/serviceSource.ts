0ae0797ee667ffdc59df20552e1d71bd3a697cd2
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 10:04:42 -0300

    save: l2/serviceSource.ts

--- l2/serviceSource.ts
+++ l2/serviceSource.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/serviceSource.ts" groupName="service" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/serviceSource.ts" enhancement="_100554_enhancementLit" />
 
 import { html } from 'lit';
 import { customElement, query, property } from 'lit/decorators.js';
