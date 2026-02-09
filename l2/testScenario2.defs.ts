d0a315a6581786fa41f285f0697384a80fc1f75c
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:33:39 -0300

    save: l2/testScenario2.defs.ts

--- l2/testScenario2.defs.ts
+++ l2/testScenario2.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/testScenario2.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/testScenario2.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
