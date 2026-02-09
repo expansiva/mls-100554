4c44464706a476fabd43956b6488f3a9201b54c1
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 11:07:34 -0300

    save: l2/ateste.defs.ts

--- l2/ateste.defs.ts
+++ l2/ateste.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/ateste.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/ateste.defs.ts" enhancement="_blank" />
 // Do not change – automatically generated code.
 export const asis: mls.defs.AsIs = {
 "meta": {
