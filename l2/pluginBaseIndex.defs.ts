2e21d6b226e579d88db9373740336e8150889c1c
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:31:07 -0300

    save: l2/pluginBaseIndex.defs.ts

--- l2/pluginBaseIndex.defs.ts
+++ l2/pluginBaseIndex.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginBaseIndex.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginBaseIndex.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
