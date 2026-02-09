422104c7fcf95e36900a07a23fedaa5cc849b0aa
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:22:54 -0300

    save: l2/agentGeneratePrototypeOrganism.defs.ts

--- l2/agentGeneratePrototypeOrganism.defs.ts
+++ l2/agentGeneratePrototypeOrganism.defs.ts
@@ -1,2 +1,2 @@
-/// <mls shortName="agentGeneratePrototypeOrganism" project="100554" enhancement="_blank" folder="" />
+/// <mls fileReference="_100554_/l2/agentGeneratePrototypeOrganism.defs.ts" enhancement="_blank" />
 
