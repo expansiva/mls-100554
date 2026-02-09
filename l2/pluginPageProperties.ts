244888aadd1e2df5649149cbd423b3cc9cd2d2c8
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 09:57:39 -0300

    save: l2/pluginPageProperties.ts

--- l2/pluginPageProperties.ts
+++ l2/pluginPageProperties.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginPageProperties.ts" groupName="other" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/pluginPageProperties.ts" enhancement="_100554_enhancementLit" />
 
 import { html, css, svg, repeat, TemplateResult } from 'lit';
 import { property, queryAll } from 'lit/decorators.js';
