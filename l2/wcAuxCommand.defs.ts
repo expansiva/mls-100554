67dae8c30c78c9aaa88c92763c8415399e1bab3d
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:33:48 -0300

    save: l2/wcAuxCommand.defs.ts

--- l2/wcAuxCommand.defs.ts
+++ l2/wcAuxCommand.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/wcAuxCommand.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/wcAuxCommand.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
