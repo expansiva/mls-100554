42921081ba9166409edfca697845345a8de11651
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:24:57 -0300

    save: l2/collabSpliterHorizontalVarFixed.defs.ts

--- l2/collabSpliterHorizontalVarFixed.defs.ts
+++ l2/collabSpliterHorizontalVarFixed.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabSpliterHorizontalVarFixed.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabSpliterHorizontalVarFixed.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
