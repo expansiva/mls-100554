8a1358952305c4e0c5a98c00e110abaff79b49b9
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:23:29 -0300

    save: l2/agents/agentDefs.ts

--- l2/agents/agentDefs.ts
+++ l2/agents/agentDefs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/agents/agentDefs" enhancement="_100554_enhancementAgent"/>
+/// <mls fileReference="_100554_/l2/agents/agentDefs.ts" enhancement="_100554_enhancementAgent" />
 
 import { IAgentAsync, IAgentMeta } from '/_100554_/l2/aiAgentBase.js';
 import { createModel } from '/_100554_/l2/collabLibModel.js';
