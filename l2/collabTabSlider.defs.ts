be97e8cb87c44adf84c392c1e87eaf9505c5efb2
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:25:12 -0300

    save: l2/collabTabSlider.defs.ts

--- l2/collabTabSlider.defs.ts
+++ l2/collabTabSlider.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/collabTabSlider.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/collabTabSlider.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
