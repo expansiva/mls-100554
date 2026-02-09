c6ba7393e46192ba06411a1ca9cce835c30098af
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:33:10 -0300

    save: l2/serviceBase.defs.ts

--- l2/serviceBase.defs.ts
+++ l2/serviceBase.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/serviceBase.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/serviceBase.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
