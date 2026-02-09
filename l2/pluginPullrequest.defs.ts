49e181a9745fb0bdb639df03fe6a84ac50d142c1
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:32:12 -0300

    save: l2/pluginPullrequest.defs.ts

--- l2/pluginPullrequest.defs.ts
+++ l2/pluginPullrequest.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginPullrequest.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/pluginPullrequest.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
