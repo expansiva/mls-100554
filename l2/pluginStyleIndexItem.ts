2eb3c3101b403666b1e855f78d4ede7505020d6c
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 09:58:16 -0300

    save: l2/pluginStyleIndexItem.ts

--- l2/pluginStyleIndexItem.ts
+++ l2/pluginStyleIndexItem.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/pluginStyleIndexItem.ts" groupName="other" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_100554_/l2/pluginStyleIndexItem.ts" enhancement="_100554_enhancementLit" />
 
 import { html, unsafeHTML } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
