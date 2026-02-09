19a96bf51de7bf32fdf62b50930cbfb3e49e4910
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 14:33:17 -0300

    save: l2/serviceLiveView.defs.ts

--- l2/serviceLiveView.defs.ts
+++ l2/serviceLiveView.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_100554_/l2/serviceLiveView.ts" enhancement="_blank" />
+/// <mls fileReference="_100554_/l2/serviceLiveView.defs.ts" enhancement="_blank" />
 
 // Do not change – automatically generated code. 
 
