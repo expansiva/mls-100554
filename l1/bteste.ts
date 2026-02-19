58448f1a989f7d054b9b65e01e6fb130d85b1159
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-19 14:24:22 -0300

    Delete l1/bteste.ts

--- l1/bteste.ts
+++
@@ -1,8 +0,0 @@
-/// <mls shortName="bteste" project="100554" folder="" enhancement="_blank" groupName="" />
-
-
-
-export function exec() {  
-    console.info('estou no bteste');
-    return 'estou no bteste'
-}
