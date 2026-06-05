d187959eddf0779ec8af3280de654f32e2aa62c6
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-06-05 17:23:26 -0300

    agent new solution

--- l2/mlsFileTree.ts
+++ l2/mlsFileTree.ts
@@ -72,9 +72,9 @@ export class MlsFileTree extends CollabLitElement {
                             <span class="icon">📁</span>
                             <div style="display:flex; gap:.5rem" .innerHTML="${node.name}"></div>
                         
-                        <span style="cursor:pointer;z-index: 99;display: flex; position: absolute; right: 25px;" title="delete all this folder" @click=${() => { this.undoAllByFolders(node.fullPath) }}><span class="fa fa-undo"></span></span>
+                        <span style="cursor:pointer;z-index: 99;display: flex; position: absolute; right: 25px;" title="undo all this folder" @click=${(e: MouseEvent) => { e.stopPropagation(); e.preventDefault(); this.undoAllByFolders(node.fullPath); }}><span class="fa fa-undo" style="pointer-events:none;"></span></span>
 
-                        <span style="cursor:pointer;z-index: 99;display: flex; position: absolute; right: 5px;" title="delete all this folder" @click=${() => { this.delAllByFolders(node.fullPath) }}><svg xmlns="http://www.w3.org/2000/svg" style="width:15px; cursor:pointer; z-index:-1" viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"/></svg></span>
+                        <span style="cursor:pointer;z-index: 99;display: flex; position: absolute; right: 5px;" title="delete all this folder" @click=${(e: MouseEvent) => { e.stopPropagation(); e.preventDefault(); this.delAllByFolders(node.fullPath); }}><svg xmlns="http://www.w3.org/2000/svg" style="width:15px; pointer-events:none;" viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"/></svg></span>
                         </info-item>
                     </div>
                 </li>
@@ -286,6 +286,11 @@ export class MlsFileTree extends CollabLitElement {
             return true;
 
         })
+
+        if (all.length === 0) return;
+
+        const folderLabel = folder === '' ? 'root' : folder;
+        if (!window.confirm(`delete: ${folderLabel} (${all.length})?`)) return;
 
         for await (const k of all) {
 
